71482abe2c9e35a8c436dce2552b32be316844de
Jostein Øygarden <jostein.oygarden@verico.com>
2017-11-16 08:18:54 +0100

    a

--- app/components/common/map/map.js
+++ app/components/common/map/map.js
@@ -48,6 +48,7 @@ export default class Map extends Component {
       <GoogleMap
         defaultZoom={this.props.defaultZoom}
         defaultCenter={this.props.defaultCenter}
+        onLoaded={(googleMaps, map) => {map.setMapTypeId(googleMaps.MapTypeId.SATELLITE)}}
       >
         <MarkerClusterer
           averageCenter={ false }
