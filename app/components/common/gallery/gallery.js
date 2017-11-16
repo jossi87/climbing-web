9c43cbeef42c301531bc39e6d460e6b4ba6f9152
Jostein Øygarden <jostein.oygarden@verico.com>
2017-11-16 19:28:46 +0100

    scale

--- app/components/common/gallery/gallery.js
+++ app/components/common/gallery/gallery.js
@@ -150,7 +150,7 @@ export default class Gallery extends Component {
       return (
         <div className='image-gallery-image'>
           <svg viewBox="0 0 841.9 595.3" style={{maxHeight: '100vh', maxWidth: '100vw', objectFit: 'scale-down', fontFamily: "'object-fit: scale-down'", overflow: 'visible'}}>
-            <image xlinkHref={config.getUrl(`images?id=${m.id}`)} x="0" y="0" width="1789" height="1195"/>
+            <image xlinkHref={config.getUrl(`images?id=${m.id}`)} x="0" y="0"/>
             {shapes}
           </svg>
         </div>
