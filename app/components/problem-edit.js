a791213310aae717261f94e441a2ee618b6c737d
Jostein Øygarden <jostein.oygarden@verico.com>
2017-12-19 12:56:05 +0100

    a

--- app/components/problem-edit.js
+++ app/components/problem-edit.js
@@ -195,7 +195,7 @@ export default class ProblemEdit extends Component {
           </FormGroup>
           <FormGroup controlId="formControlsFaDate">
             <ControlLabel>FA date</ControlLabel><br/>
-            <DatePicker value={this.state.faDate} onChange={this.onFaDateChanged.bind(this)} style={{ zIndex: '1'}} /><br/>
+            <DatePicker calendarType="ISO 8601" value={this.state.faDate} onChange={this.onFaDateChanged.bind(this)} style={{ zIndex: '9'}} /><br/>
             <ButtonGroup>
               <Button onClick={this.onFaDateChanged.bind(this, yesterday.toISOString().substring(0,10))}>Yesterday</Button>
               <Button onClick={this.onFaDateChanged.bind(this, new Date().toISOString().substring(0,10))}>Today</Button>
