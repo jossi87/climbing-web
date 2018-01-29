1f69c20c7c0b6994218991ff1af4c9d2b10b7dd8
Jostein Øygarden <jostein.oygarden@verico.com>
2018-01-29 12:46:35 +0100

    x

--- app/components/browse.js
+++ app/components/browse.js
@@ -8,23 +8,7 @@ import auth from '../utils/auth.js';
 import config from '../utils/config.js';
 
 class TableRow extends Component {
-  toRad(value) {
-    return value * Math.PI / 180;
-  }
 
-  calcCrow(lat1, lon1, lat2, lon2) {
-    var R = 6371; // km
-    var dLat = this.toRad(lat2-lat1);
-    var dLon = this.toRad(lon2-lon1);
-    var lat1 = this.toRad(lat1);
-    var lat2 = this.toRad(lat2);
-
-    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-      Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    var d = R * c;
-    return d;
-  }
 
   render() {
     var comment = "";
@@ -38,7 +22,7 @@ class TableRow extends Component {
     }
     var distance = "";
     if (this.props.area.currLat>0 && this.props.area.currLng>0 && this.props.area.lat>0 && this.props.area.lng>0) {
-      distance = this.calcCrow(this.props.area.currLat, this.props.area.currLng, this.props.area.lat, this.props.area.lng).toFixed(1) + " km";
+      distance = " km";
     }
     return (
       <tr>
