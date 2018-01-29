ca9034eff48bbda1d2fef913c69b9c966fc4f61d
Jostein Øygarden <jostein.oygarden@verico.com>
2018-01-29 12:33:32 +0100

    distance

--- app/components/browse.js
+++ app/components/browse.js
@@ -8,6 +8,24 @@ import auth from '../utils/auth.js';
 import config from '../utils/config.js';
 
 class TableRow extends Component {
+  toRad(value) {
+    return value * Math.PI / 180;
+  }
+
+  calcCrow(lat1, lon1, lat2, lon2) {
+    var R = 6371; // km
+    var dLat = this.toRad(lat2-lat1);
+    var dLon = this.toRad(lon2-lon1);
+    var lat1 = this.toRad(lat1);
+    var lat2 = this.toRad(lat2);
+
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c;
+    return d;
+  }
+
   render() {
     var comment = "";
     if (this.props.area.comment) {
@@ -45,24 +63,6 @@ export default class Browse extends Component {
       });
       document.title=config.getTitle() + " | browse";
     });
-  }
-
-  toRad(value) {
-    return value * Math.PI / 180;
-  }
-
-  calcCrow(lat1, lon1, lat2, lon2) {
-    var R = 6371; // km
-    var dLat = this.toRad(lat2-lat1);
-    var dLon = this.toRad(lon2-lon1);
-    var lat1 = this.toRad(lat1);
-    var lat2 = this.toRad(lat2);
-
-    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-      Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    var d = R * c;
-    return d;
   }
 
   render() {
