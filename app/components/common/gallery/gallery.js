ac3590540fee5984737bc4464d45e3056f619c8e
Jostein Øygarden <jostein.oygarden@verico.com>
2018-01-30 10:15:50 +0100

    svg

--- app/components/common/gallery/gallery.js
+++ app/components/common/gallery/gallery.js
@@ -140,14 +140,14 @@ export default class Gallery extends Component {
     if (m.svgs) {
       const shapes = [];
       for (let svg of m.svgs) {
-        shapes.push(<text key={shapes.length} transform={svg.textTransform} style={{fill: '#FFFFFF', fontSize: '72px'}}>{svg.nr}</text>);
+        if (svg.nrPathD) {
+          shapes.push(<path key={shapes.length} d={svg.nrPathD}  style={{fill: '#E2011A', strokeWidth: '10'}}/>);
+        }
         shapes.push(<path key={shapes.length} d={svg.linePathD}  style={{fill: 'none', stroke: '#E2011A', strokeWidth: '10', strokeDasharray: '20'}}/>);
         if (svg.topPathD) {
           shapes.push(<path key={shapes.length} d={svg.topPathD}  style={{fill: '#E2011A', strokeWidth: '10'}}/>);
         }
-        if (svg.nrPathD) {
-          shapes.push(<path key={shapes.length} d={svg.nrPathD}  style={{fill: '#E2011A', strokeWidth: '10'}}/>);
-        }
+        shapes.push(<text key={shapes.length} transform={svg.textTransform} style={{fill: '#FFFFFF', fontSize: '72px'}}>{svg.nr}</text>);
       };
       return (
         <div className='image-gallery-image'>
