5d70a50d74535ca68d589be2e368db570971c472
Jostein Øygarden <jostein.oygarden@verico.com>
2018-02-07 09:41:27 +0100

    svg-edit

--- app/components/common/svg/svg-edit.js
+++ app/components/common/svg/svg-edit.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Well, FormGroup, FormControl, MenuItem, ButtonToolbar, ButtonGroup, Button, DropdownButton, ControlLabel } from 'react-bootstrap';
+import { Well, FormGroup, FormControl, MenuItem, ButtonToolbar, ButtonGroup, Button, DropdownButton, ControlLabel, Alert } from 'react-bootstrap';
 
 export default class SvgEdit extends Component {
   componentDidMount() {
@@ -119,6 +119,54 @@ export default class SvgEdit extends Component {
     }
   };
 
+  setPointType(v) {
+    const points = this.state.points;
+    const active = this.state.activePoint;
+    if (active !== 0) { // not the first point
+      switch (v) {
+        case "L":
+            points[active] = {x: points[active].x, y: points[active].y};
+            break;
+        case "C":
+            points[active] = {
+              x: points[active].x,
+              y: points[active].y,
+              c: [
+                  {
+                    x: (points[active].x + points[active - 1].x - 50) / 2,
+                    y: (points[active].y + points[active - 1].y) / 2
+                  },
+                  {
+                    x: (points[active].x + points[active - 1].x + 50) / 2,
+                    y: (points[active].y + points[active - 1].y) / 2
+                  }
+              ]
+            };
+        break;
+      }
+      this.setState({points});
+    }
+  };
+
+  removeActivePoint(e) {
+    let points = this.state.points;
+    let active = this.state.activePoint;
+    if (points.length > 1 && active !== 0) {
+      points.splice(active, 1);
+      this.setState({points, activePoint: points.length-1});
+    }
+  };
+
+  reset(e) {
+    this.setState({
+      ctrl: false,
+      points: [],
+      activePoint: 0,
+      draggedPoint: false,
+      draggedCubic: false
+    });
+  };
+
   render() {
     if (!this.state) {
       return <center><i className="fa fa-cog fa-spin fa-2x"></i></center>;
@@ -142,14 +190,40 @@ export default class SvgEdit extends Component {
         </g>
       );
     });
-
+    const path = this.generatePath();
     return (
       <Well onMouseUp={this.cancelDragging.bind(this)}>
-        <svg viewBox={"0 0 " + this.state.w + " " + this.state.h} className="buldreinfo-svg" onClick={this.addPoint.bind(this)} onMouseMove={this.handleMouseMove.bind(this)}>
-          <image ref="buldreinfo-svg-edit-img" xlinkHref="https://brattelinjer.no/com.buldreinfo.jersey.jaxb/v1/images?id=20264" x="0" y="0" width="100%" height="100%"/>
-          <path className="buldreinfo-svg-edit-route" d={this.generatePath()} />
-          <g>{circles}</g>
-        </svg>
+        <form>
+          <FormGroup controlId="formControlsInfo">
+            <Alert bsStyle="info">
+              <strong>CLICK</strong> to select a point. <strong>CLICK AND DRAG</strong> to move a point. <strong>CTRL + CLICK</strong> to add a point.
+            </Alert>
+          </FormGroup>
+          <FormGroup controlId="formControlsSettings">
+            <ButtonGroup>
+              {this.state.activePoint !== 0 && (
+                <DropdownButton title={!!this.state.points[this.state.activePoint].c? "Selected point: Curve to" : "Selected point: Line to"} id="bg-nested-dropdown">
+                  <MenuItem eventKey="0" onSelect={this.setPointType.bind(this, "L")}>Selected point: Line to</MenuItem>
+                  <MenuItem eventKey="1" onSelect={this.setPointType.bind(this, "C")}>Selected point: Curve to</MenuItem>
+                </DropdownButton>
+              )}
+              {this.state.activePoint !== 0 && (
+                <Button onClick={this.removeActivePoint.bind(this)}>Remove this point</Button>
+              )}
+              <Button bsStyle="warning" disabled={this.state.points.length===0} onClick={this.reset.bind(this)}>Reset path</Button>
+            </ButtonGroup>
+          </FormGroup>
+          <FormGroup controlId="formControlsImage">
+            <svg viewBox={"0 0 " + this.state.w + " " + this.state.h} className="buldreinfo-svg" onClick={this.addPoint.bind(this)} onMouseMove={this.handleMouseMove.bind(this)}>
+              <image ref="buldreinfo-svg-edit-img" xlinkHref="https://brattelinjer.no/com.buldreinfo.jersey.jaxb/v1/images?id=20264" x="0" y="0" width="100%" height="100%"/>
+              <path className="buldreinfo-svg-edit-route" d={path} />
+              <g>{circles}</g>
+            </svg>
+          </FormGroup>
+          <FormGroup controlId="formControlsPath">
+            {path}
+          </FormGroup>
+        </form>
       </Well>
     )
   }
