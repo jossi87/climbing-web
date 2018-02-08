cfd63198c8ebacb8042382a612a9cd98e12ca7e0
Jostein Øygarden <jostein.oygarden@verico.com>
2018-02-08 08:27:01 +0100

    svg

--- app/components/common/svg/svg-edit.js
+++ app/components/common/svg/svg-edit.js
@@ -60,7 +60,6 @@ export default class SvgEdit extends Component {
   }
 
   cancelDragging(e) {
-    console.log("cancelDragging");
     this.setState({
       draggedPoint: false,
       draggedCubic: false
