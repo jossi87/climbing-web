130ca33de53b2090ed9a706ac79e91f3ddb7b586
Jostein Øygarden <jostein.oygarden@verico.com>
2018-02-13 12:50:19 +0100

    x

--- app/components/common/gallery/gallery.js
+++ app/components/common/gallery/gallery.js
@@ -195,7 +195,7 @@ export default class Gallery extends Component {
     if (m.svgs) {
       return (
         <div className='image-gallery-image'>
-          <svg className="buldreinfo-svg" viewBox={"0 0 " + m.width + " " + m.height} preserveAspectRatio="none slice">
+          <svg className="buldreinfo-svg" viewBox={"0 0 " + m.width + " " + m.height} preserveAspectRatio="none slice" width="100%">
             <image xlinkHref={config.getUrl(`images?id=${m.id}`)} width="100%" height="100%"/>
             {this.generateShapes(m.svgs, m.svgProblemId, m.width, m.height)}
           </svg>
