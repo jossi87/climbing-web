3c8e85cca7b2eddc4cf8a391f968898d68f4f3d2
Jostein Øygarden <jostein.oygarden@verico.com>
2018-02-22 15:48:40 +0100

    x

--- app/components/sector.js
+++ app/components/sector.js
@@ -133,11 +133,14 @@ export default class Sector extends Component {
     const problemsInTopo = [];
     if (this.state.media) {
       this.state.media.forEach(m => {
+        console.log("media=" + m);
         if (m.svgs) {
           this.state.media.forEach(svg => problemsInTopo.push(svg.problemId));
+          console.log("svgs=" + m.svgs);
         }
       });
     }
+    console.log(problemsInTopo);
 
     const rows = this.state.problems.map((problem, i) => {
       return (
