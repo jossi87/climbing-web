f26f8cfad6bf1c0649ad4eae6671db5922276c97
Jostein Øygarden <jostein.oygarden@verico.com>
2018-03-22 10:25:58 +0100

    webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -2,38 +2,35 @@ const webpack = require('webpack');
 const HtmlPlugin = require('html-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 module.exports = {
-  // Tell webpack to start bundling our app at app/index.js
-  entry: './app',
-  // Output our app to the dist/ directory
-  output: {
-    publicPath: '/',
-    filename: 'index.js',
-    path: __dirname + '/public'
-  },
-  // Emit source maps so we can debug our code in the browser
-  devtool: 'source-map',
-  // Tell webpack to run our source code through Babel
+  entry: [
+    './app'
+  ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    },
-    {
-      test: /\.css$/,
-      use: [
-        { loader: 'style-loader' },
-        { loader: 'css-loader' }
-      ]
-    }]
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ]
+      }
+    ]
   },
-  // Since Webpack only understands JavaScript, we need to
-  // add a plugin to tell it how to handle html files.
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/public',
+    publicPath: '/',
+    filename: 'index.js'
+  },
+  devtool: 'source-map',
   plugins: [
-    // Configure HtmlPlugin to use our own index.html file
-    // as a template.
-    // Check out https://github.com/jantimon/html-webpack-plugin
-    // for the full list of options.
     new HtmlPlugin({
       template: 'app/index.html'
     }),
@@ -47,4 +44,4 @@ module.exports = {
       }
     })
   ]
-}
+};
