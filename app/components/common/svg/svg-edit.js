df9f50f5bc49889f96632a2ceb9521fd3fd24b7d
Jostein Øygarden <jostein.oygarden@verico.com>
2018-03-28 13:21:05 +0200

    anchor

--- app/components/common/svg/svg-edit.js
+++ app/components/common/svg/svg-edit.js
@@ -75,7 +75,13 @@ export default class SvgEdit extends Component {
   };
 
   toggleAnchor() {
-    this.setState({hasAnchor: !this.state.hasAnchor});
+    var anchor = this.state.hasAnchor;
+    if (anchor === true) {
+      anchor = false;
+    } else {
+      anchor = true;
+    }
+    this.setState({hasAnchor: anchor});
   }
 
   onCancel() {
@@ -266,7 +272,7 @@ export default class SvgEdit extends Component {
     if (y < r) y = r;
     if (y > (this.state.h-r)) y = this.state.h-r;
     var anchor = null;
-    if (hasAnchor) {
+    if (hasAnchor === true) {
       anchor = <circle className="buldreinfo-svg-ring" cx={path[ixAnchor].x} cy={path[ixAnchor].y} r={0.006*this.state.w}/>
     }
     return (
@@ -345,7 +351,7 @@ export default class SvgEdit extends Component {
                     {this.state.activePoint !== 0 && (
                       <Button onClick={this.removeActivePoint.bind(this)}>Remove this point</Button>
                     )}
-                    <DropdownButton title={this.state.hasAnchor? "Route has anchor" : "No anchor on route"} disabled={this.state.points.length===0} id="bg-nested-dropdown">
+                    <DropdownButton title={this.state.hasAnchor === true? "Route has anchor" : "No anchor on route"} disabled={this.state.points.length===0} id="bg-nested-dropdown">
                       <MenuItem eventKey="0" onSelect={this.toggleAnchor.bind(this)}>No anchor on route</MenuItem>
                       <MenuItem eventKey="1" onSelect={this.toggleAnchor.bind(this)}>Route has anchor</MenuItem>
                     </DropdownButton>
