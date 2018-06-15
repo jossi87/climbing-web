90f102f1357c72022f056eeb74ed84e5ca842aa8
Jostein Øygarden <jostein.oygarden@verico.com>
2018-06-15 09:28:35 +0200

    x

---
+++ app/components/common/problem-section/problem-section.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import createClass from 'create-react-class';
+import PropTypes from 'prop-types';
+import { FormGroup, ControlLabel, FormControl, DropdownButton, MenuItem, Well } from 'react-bootstrap';
+
+var ProblemSection = createClass({
+	displayName: 'ProblemSection',
+	propTypes: {
+		label: PropTypes.string
+	},
+	getInitialState() {
+    return {
+      sections: this.props.sections,
+      numberOfSections: this.props.sections? this.props.sections.length : 1
+    };
+	},
+	render() {
+		return (
+      <Well>
+        <FormGroup controlId="formControlsSections">
+          <ControlLabel>Sections</ControlLabel>
+          <DropdownButton title={this.state.numberOfSections} id="bg-nested-dropdown">
+            <MenuItem key={1} eventKey={1} onSelect={this.onNumberOfSectionsChange.bind(this, 1)}>1</MenuItem>
+            <MenuItem key={2} eventKey={2} onSelect={this.onNumberOfSectionsChange.bind(this, 2)}>2</MenuItem>
+            <MenuItem key={3} eventKey={3} onSelect={this.onNumberOfSectionsChange.bind(this, 3)}>3</MenuItem>
+            <MenuItem key={4} eventKey={4} onSelect={this.onNumberOfSectionsChange.bind(this, 4)}>4</MenuItem>
+            <MenuItem key={5} eventKey={5} onSelect={this.onNumberOfSectionsChange.bind(this, 5)}>5</MenuItem>
+            <MenuItem key={6} eventKey={6} onSelect={this.onNumberOfSectionsChange.bind(this, 6)}>6</MenuItem>
+            <MenuItem key={7} eventKey={7} onSelect={this.onNumberOfSectionsChange.bind(this, 7)}>7</MenuItem>
+            <MenuItem key={8} eventKey={8} onSelect={this.onNumberOfSectionsChange.bind(this, 8)}>8</MenuItem>
+            <MenuItem key={9} eventKey={9} onSelect={this.onNumberOfSectionsChange.bind(this, 9)}>9</MenuItem>
+            <MenuItem key={10} eventKey={10} onSelect={this.onNumberOfSectionsChange.bind(this, 10)}>10</MenuItem>
+          </DropdownButton>
+        </FormGroup>
+      </Well>
+		);
+	}
+});
+
+module.exports = ProblemSection;
