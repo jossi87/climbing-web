78a1c580ecd6dd2a5c44a638de68121b7a91ed09
Jostein Øygarden <jostein.oygarden@verico.com>
2018-07-25 09:27:25 +0200

    x

--- app/components/app.js
+++ app/components/app.js
@@ -1,9 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom'; // Used for navbar hack
 import {Route,  Switch} from 'react-router-dom';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCamera, faCheck, faComment, faEdit, faHashtag, faImage, faLock, faMapMarker, faPlane, faPlusSquare, faSpinner, faStar, faStarHalf, faTrash, faUserSecret, faVideo } from '@fortawesome/free-solid-svg-icons';
 import Loadable from 'react-loadable';
 import Loading from './common/loading/loading';
 import Navigation from './navigation';
@@ -26,8 +23,27 @@ const SvgEdit = Loadable({loader: () => import('./common/svg/svg-edit'), loading
 const User = Loadable({loader: () => import('./user'), loading: Loading});
 const UserEdit = Loadable({loader: () => import('./user-edit'), loading: Loading});
 
-export default class App extends Component {
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCamera, faCheck, faComment, faEdit, faHashtag, faImage, faLock, faMapMarker, faPlane, faPlusSquare, faSpinner, faStar, faStarHalf, faTrash, faUserSecret, faVideo } from '@fortawesome/free-solid-svg-icons';
+library.add(faCamera);
+library.add(faCheck);
+library.add(faComment);
+library.add(faEdit);
+library.add(faHashtag);
+library.add(faImage);
+library.add(faLock);
+library.add(faMapMarker);
+library.add(faPlane);
+library.add(faPlusSquare);
+library.add(faSpinner);
+library.add(faStar);
+library.add(faStarHalf);
+library.add(faTrash);
+library.add(faUserSecret);
+library.add(faVideo);
 
+export default class App extends Component {
   // Temp fix to collapse nav-button on devices: https://github.com/lefant/react-bootstrap/commit/c68b46baea + https://github.com/react-bootstrap/react-router-bootstrap/issues/112#issuecomment-142599003
   componentDidMount() {
     const navBar = ReactDOM.findDOMNode(this).querySelector('nav.navbar');
@@ -40,23 +56,6 @@ export default class App extends Component {
       }
       btnToggle.click();
     }, false);
-
-    library.add(faCamera);
-    library.add(faCheck);
-    library.add(faComment);
-    library.add(faEdit);
-    library.add(faHashtag);
-    library.add(faImage);
-    library.add(faLock);
-    library.add(faMapMarker);
-    library.add(faPlane);
-    library.add(faPlusSquare);
-    library.add(faSpinner);
-    library.add(faStar);
-    library.add(faStarHalf);
-    library.add(faTrash);
-    library.add(faUserSecret);
-    library.add(faVideo);
   }
 
   render() {
