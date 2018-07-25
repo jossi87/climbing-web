b98ef552588618bd6ac83687ade97ac45ac1cfc3
Jostein Øygarden <jostein.oygarden@verico.com>
2018-07-25 11:55:07 +0200

    x

--- app/components/navigation.js
+++ app/components/navigation.js
@@ -20,7 +20,7 @@ const CustomOption = (props) => {
   return (
     <components.Option {...props}>
       <div>
-        <Avatar value={props.value.avatar? props.value.avatar : "7A"} size={25} color={bg} round={true} textSizeRatio={2.25} />
+        <Avatar value={props.value.avatar? props.value.avatar : "7A"} size={25} color={bg} round={true} textSizeRatio={2.25} style={{marginRight: '10px'}} />
         {props.label} {props.value.visibility===1 && <FontAwesomeIcon icon="lock" />}{props.value.visibility===2 && <FontAwesomeIcon icon="user-secret" />}
       </div>
     </components.Option>
