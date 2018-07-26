720e31c0fa195af428d8a320838087659017aa78
Jostein Øygarden <jostein.oygarden@verico.com>
2018-07-26 13:52:10 +0200

    prod

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('development') // development / production
+        'NODE_ENV': JSON.stringify('production') // development / production
       }
     })
   ]
