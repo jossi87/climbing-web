12aff0d568f67db70b6b430d5d22498f480822e0
Jostein Øygarden <jostein.oygarden@verico.com>
2018-07-26 17:42:47 +0200

    x

--- app/components/common/map/map.js
+++ app/components/common/map/map.js
@@ -23,68 +23,71 @@ export default class Map extends Component {
       return (<Redirect to={this.state.pushUrl} push />);
     }
 
-    var markers = null;
-    if (this.props.markers) {
-      markers = this.props.markers.map((m, i) => {
-        var myIcon = {};
-        if (m.icon) {
-          if (m.icon.url) {
-            myIcon.url = m.icon.url;
+    const GettingStartedGoogleMap = withScriptjs(withGoogleMap(props => {
+      var markers = null;
+      if (this.props.markers) {
+        markers = this.props.markers.map((m, i) => {
+          var myIcon = {};
+          if (m.icon) {
+            if (m.icon.url) {
+              myIcon.url = m.icon.url;
+            }
+            if (m.icon.scaledSizeW && m.icon.scaledSizeH) {
+              myIcon.scaledSize = new google.maps.Size(m.icon.scaledSizeW, m.icon.scaledSizeH);
+            }
+            if (m.icon.labelOriginX && m.icon.labelOriginY) {
+              myIcon.labelOrigin = new google.maps.Point(m.icon.labelOriginX, m.icon.labelOriginY);
+            }
           }
-          if (m.icon.scaledSizeW && m.icon.scaledSizeH) {
-            myIcon.scaledSize = new google.maps.Size(m.icon.scaledSizeW, m.icon.scaledSizeH);
-          }
-          if (m.icon.labelOriginX && m.icon.labelOriginY) {
-            myIcon.labelOrigin = new google.maps.Point(m.icon.labelOriginX, m.icon.labelOriginY);
-          }
-        }
-        return (
-          <Marker
-            icon={myIcon}
-            key={i}
-            position={{lat: m.lat, lng: m.lng}}
-            label={m.label}
-            title={m.title}
-            clickable={true}
-            onClick={this.handleOnClick.bind(this, m.url)}/>
-        );
-      });
-    }
-    if (this.state && this.state.currLat && this.state.currLng && this.state.currLat>0 && this.state.currLng>0) {
-      markers.push(<Marker
-                    key={-1}
-                    icon="https://maps.gstatic.com/mapfiles/markers2/measle_blue.png"
-                    position={{lat: this.state.currLat, lng: this.state.currLng}}/>)
-    }
-    var polygons = null;
-    if (this.props.polygons) {
-      polygons = this.props.polygons.map((p, i) => {
-        return (
-          <Polygon
-            key={i}
-            paths={p.triangleCoords}
-            options={{strokeColor: '#FF3300', strokeOpacity: '0.5', strokeWeight: '2', fillColor: '#FF3300', fillOpacity: '0.15'}}
-            onClick={this.handleOnClick.bind(this, p.url)}/>
-        );
-      });
-    }
-    const GettingStartedGoogleMap = withScriptjs(withGoogleMap(props => (
-      <GoogleMap
-        defaultZoom={this.props.defaultZoom}
-        defaultCenter={this.props.defaultCenter}
-        defaultMapTypeId={google.maps.MapTypeId.TERRAIN}
-      >
-        <MarkerClusterer
-          averageCenter={ false }
-          minimumClusterSize={ 60 }
-          enableRetinaIcons={ false }
-          imagePath={ "https://raw.githubusercontent.com/googlemaps/js-marker-clusterer/gh-pages/images/m" }
-          gridSize={ 60 }>
-          {markers}
-          {polygons}
-        </MarkerClusterer>
-      </GoogleMap>
-    )));
+          return (
+            <Marker
+              icon={myIcon}
+              key={i}
+              position={{lat: m.lat, lng: m.lng}}
+              label={m.label}
+              title={m.title}
+              clickable={true}
+              onClick={this.handleOnClick.bind(this, m.url)}/>
+          );
+        });
+      }
+      if (this.state && this.state.currLat && this.state.currLng && this.state.currLat>0 && this.state.currLng>0) {
+        markers.push(<Marker
+                      key={-1}
+                      icon="https://maps.gstatic.com/mapfiles/markers2/measle_blue.png"
+                      position={{lat: this.state.currLat, lng: this.state.currLng}}/>)
+      }
+      var polygons = null;
+      if (this.props.polygons) {
+        polygons = this.props.polygons.map((p, i) => {
+          return (
+            <Polygon
+              key={i}
+              paths={p.triangleCoords}
+              options={{strokeColor: '#FF3300', strokeOpacity: '0.5', strokeWeight: '2', fillColor: '#FF3300', fillOpacity: '0.15'}}
+              onClick={this.handleOnClick.bind(this, p.url)}/>
+          );
+        });
+      }
+      
+      return (
+        <GoogleMap
+          defaultZoom={this.props.defaultZoom}
+          defaultCenter={this.props.defaultCenter}
+          defaultMapTypeId={google.maps.MapTypeId.TERRAIN}
+        >
+          <MarkerClusterer
+            averageCenter={ false }
+            minimumClusterSize={ 60 }
+            enableRetinaIcons={ false }
+            imagePath={ "https://raw.githubusercontent.com/googlemaps/js-marker-clusterer/gh-pages/images/m" }
+            gridSize={ 60 }>
+            {markers}
+            {polygons}
+          </MarkerClusterer>
+        </GoogleMap>
+      );
+    }));
 
     return (
       <section style={{height: '600px'}}>
