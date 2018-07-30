b6a0479c50d1df2112d55e8bcda0346a3faaba14
Jostein Øygarden <jostein.oygarden@verico.com>
2018-07-30 15:58:40 +0200

    all files

---
+++ src/shared/utils/util.js
@@ -0,0 +1,15 @@
+module.exports = {
+  getImageUrl(id, maxHeight) {
+    if (maxHeight) {
+      return encodeURI(`https://buldreinfo.com/com.buldreinfo.jersey.jaxb/v1/images?id=${id}&targetHeight=${maxHeight}`);
+    }
+    return encodeURI(`https://buldreinfo.com/com.buldreinfo.jersey.jaxb/v1/images?id=${id}`);
+  },
+
+  convertFromDateToString(date) {
+    var d = date.getDate();
+    var m = date.getMonth() + 1;
+    var y = date.getFullYear();
+    return y + '-' + (m <= 9 ? '0' + m : m) + '-' + (d <= 9 ? '0' + d : d);
+  }
+}
