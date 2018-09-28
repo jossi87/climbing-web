920816c477de5558f28d396518312e85bcfcd0e2
jostein.oygarden <jostein.oygarden@verico.com>
2018-09-28 12:32:57 +0200

    removed .js

--- src/shared/components/common/imagebox/imagebox.tsx
+++ src/shared/components/common/imagebox/imagebox.tsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
-import { getImageUrl } from '../../../api.js';
+import { getImageUrl } from '../../../api';
 
 export default class ImageBox extends Component<any, any> {
   constructor(props) {
