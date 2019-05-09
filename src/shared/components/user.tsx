db6f694defee9d81432b2b0808d3a81fca716374
Jostein Øygarden <jostein.oygarden@gmail.com>
2019-05-09 14:40:31 +0200

    x

--- src/shared/components/user.tsx
+++ src/shared/components/user.tsx
@@ -109,7 +109,7 @@ class User extends Component<any, any> {
             <Label color='blue' image><Icon name='video' />{numberWithCommas(data.numVideoTags)}<Label.Detail>Tag</Label.Detail></Label>
             <Label color='violet' image><Icon name='video' />{numberWithCommas(data.numVideosCreated)}<Label.Detail>Captured</Label.Detail></Label>
           </Label.Group>
-          {this.props.isAuthenticated && this.props.match.params.userId==data.id &&
+          {this.props.isAuthenticated && (this.props.match.params.userId==data.id || !this.props.match.params.userId) &&
             <>
               <br/>
               <Checkbox
