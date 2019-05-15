8fe884cc98a5d123f6cb481cb39e2695a9bab879
Jostein Øygarden <jostein.oygarden@gmail.com>
2019-05-15 08:10:44 +0200

    hooks

--- src/shared/components/common/chart-grade-distribution/chart-grade-distribution.tsx
+++ src/shared/components/common/chart-grade-distribution/chart-grade-distribution.tsx
@@ -1,48 +1,39 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect } from 'react';
 import { LoadingAndRestoreScroll } from '../widgets/widgets';
 import { getGradeDistribution } from './../../../api';
 
-class ChartGradeDistribution extends Component<any, any> {
-  componentDidMount() {
-    this.refresh();
-  }
+const ChartGradeDistribution = ({auth, idArea, idSector}) => {
+  const [gradeDistribution, setGradeDistribution] = useState([]);
 
-  componentDidUpdate() {
-    this.refresh();
-  }
-  
-  refresh() {
-    getGradeDistribution(this.props.auth.getAccessToken(), this.props.idArea, this.props.idSector).then((res) => {
-      this.setState({ gradeDistribution: res });
+  useEffect(() => {
+    getGradeDistribution(auth.getAccessToken(), idArea, idSector).then((res) => {
+      setGradeDistribution(res);
     });
-  }
+  }, [auth, idArea, idSector]);
 
-  render() {
-    if (!this.state) {
-      return <LoadingAndRestoreScroll />;
-    }
-    const { gradeDistribution } = this.state;
-    const maxValue = Math.max.apply(Math, gradeDistribution.map(d => {return d.num}));
-    const cols = gradeDistribution.map((g, i) => {
-      const h = (g.num/maxValue*90) + '%';
-      return (
-          <td key={i} style={{height: '100%', verticalAlign: 'bottom', textAlign: 'center'}}>
-            {g.num}
-            <div style={{marginLeft: '3px', marginRight: '3px', height: h, backgroundColor: '#3182bd'}}></div>
-          </td>
-      )
-    });
+  if (!gradeDistribution) {
+    return <LoadingAndRestoreScroll />;
+  }
+  const maxValue = Math.max.apply(Math, gradeDistribution.map(d => {return d.num}));
+  const cols = gradeDistribution.map((g, i) => {
+    const h = (g.num/maxValue*90) + '%';
     return (
-      <table style={{height: '20vh'}}>
-        <tr>
-          {cols}
-        </tr>
-        <tr>
-          {gradeDistribution.map((g, i) => <td style={{width: '40px', textAlign: 'center'}} key={i}><strong>{g.grade}</strong></td>)}
-        </tr>
-      </table>
+        <td key={i} style={{height: '100%', verticalAlign: 'bottom', textAlign: 'center'}}>
+          {g.num}
+          <div style={{marginLeft: '3px', marginRight: '3px', height: h, backgroundColor: '#3182bd'}}></div>
+        </td>
     )
-  }
+  });
+  return (
+    <table style={{height: '20vh'}}>
+      <tr>
+        {cols}
+      </tr>
+      <tr>
+        {gradeDistribution.map((g, i) => <td style={{width: '40px', textAlign: 'center'}} key={i}><strong>{g.grade}</strong></td>)}
+      </tr>
+    </table>
+  )
 }
 
 export default ChartGradeDistribution;
