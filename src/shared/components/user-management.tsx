3fb679ba0fb2064bfd8e13422c271f82d617121f
Jostein Øygarden <jostein.oygarden@gmail.com>
2019-07-15 17:06:37 +0200

    x

--- src/shared/components/user-management.tsx
+++ src/shared/components/user-management.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import MetaTags from 'react-meta-tags';
-import { LoadingAndRestoreScroll } from './common/widgets/widgets';
+import { LoadingAndRestoreScroll, LockSymbol } from './common/widgets/widgets';
 import { getManagementUsers, postManagementUsers } from '../api';
-import { Segment, Breadcrumb, List, Image, Dropdown } from 'semantic-ui-react'
+import { Segment, Header, List, Image, Dropdown } from 'semantic-ui-react';
 
 const UserManagement = ({auth}) => {
   const [userManagement, setUserManagement] = useState([]);
@@ -22,23 +22,20 @@ const UserManagement = ({auth}) => {
         <title>User management</title>
         <meta name="description" content={"User management"} />
       </MetaTags>
-      <Breadcrumb>
-        <Breadcrumb.Section active>User management</Breadcrumb.Section>
-      </Breadcrumb>
+      <Header as="h2">User management</Header>
       <List>
         {userManagement.map((u, key) => (
           <List.Item key={key}>
             <Image avatar src={u.picture? u.picture : '/png/image.png'} />
             <List.Content>
-              <List.Header>{u.name}</List.Header>
+              <List.Header>{u.name} <LockSymbol visibility={u.write}/></List.Header>
               <List.Description>
-                Last seen {u.lastLogin}<br/>
-                <Dropdown selection value={u.write}
+                <Dropdown value={u.write}
                   disabled={u.readOnly}
                   options={[
-                    {key: 0, value: 0, text: "Default user"},
-                    {key: 1, value: 1, text: "Administrator"},
-                    {key: 2, value: 2, text: "Super administrator (can mange users)"}
+                    {key: 0, value: 0, icon: "user", text: "Default user"},
+                    {key: 1, value: 1, icon: "lock", text: "Administrator"},
+                    {key: 2, value: 2, icon: "user secret", text: "Super administrator (can mange users)"}
                   ]}
                   onChange={(e, data) => {
                     u.write=data.value;
@@ -50,7 +47,9 @@ const UserManagement = ({auth}) => {
                       console.warn(error);
                       alert(error.toString());
                     });
-                  }}/>
+                  }}
+                /><br/>
+                <i>Last seen {u.lastLogin}</i>
               </List.Description>
             </List.Content>
           </List.Item>
