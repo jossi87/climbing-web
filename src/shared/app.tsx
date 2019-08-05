ce9fb0e795dadf2e8e786463b0ec3c8d195c5dee
Jostein Øygarden <jostein.oygarden@gmail.com>
2019-08-05 19:21:06 +0200

    narvik

--- src/shared/app.tsx
+++ src/shared/app.tsx
@@ -94,6 +94,7 @@ class App extends Component {
                   <List.Item as='a' href='https://brattelinjer.no' rel='noopener' target='_blank'>brattelinjer.no</List.Item>
                   <List.Item as='a' href='https://klatring.jotunheimenfjellsport.com' rel='noopener' target='_blank'>klatring.jotunheimenfjellsport.com</List.Item>
                   <List.Item as='a' href='https://klatreforer.tromsoklatring.no' rel='noopener' target='_blank'>klatreforer.tromsoklatring.no</List.Item>
+                  <List.Item as='a' href='https://klatreforer.narvikklatreklubb.no' rel='noopener' target='_blank'>klatreforer.narvikklatreklubb.no</List.Item>
                   </List>
                 </Grid.Column>
                 <Grid.Column width={4}>
