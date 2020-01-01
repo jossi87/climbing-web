61ec0675b338abb0a8d55ad5d28626e37c77f999
jossi <jossi@DESKTOP-BGH357U.lan>
2020-01-01 15:32:00 +0100

    x

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Analytics from 'react-router-ga';
 import history from "./utils/history";
+import { Router } from 'react-router-dom';
 import { Auth0Provider } from "./utils/react-auth0-spa";
 import App from './app';
 
@@ -17,9 +19,13 @@ const Index = () => (
       domain='climbing.eu.auth0.com'
       client_id='DNJNVzhxbF7PtaBFh7H6iBSNLh2UJWHt'
       redirect_uri={window.location.origin}
-      onRedirectCallback={() => {onRedirectCallback}}
+      onRedirectCallback={onRedirectCallback}
     >
-    <App />
+    <Router history={history}>
+      <Analytics id="UA-76534258-1">
+        <App />
+      </Analytics>
+    </Router>
   </Auth0Provider>
 );
 
