b7bdc3596f59b09a5b5ceae53f26150b3e670eda
Jostein Øygarden <jostein.oygarden@gmail.com>
2020-01-04 18:07:28 +0100

    x

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { useHistory } from 'react-router-dom';
 import { Auth0Provider } from './utils/react-auth0-spa';
 import App from './app';
 
-const onRedirectCallback = appState => {
-  let history = useHistory();
-  history.push(
-    appState && appState.targetUrl
-      ? appState.targetUrl
-      : window.location.pathname
-  );
-};
-
 const Index = () => (
-
   <Auth0Provider
       domain='climbing.eu.auth0.com'
       client_id='DNJNVzhxbF7PtaBFh7H6iBSNLh2UJWHt'
       redirect_uri={window.location.origin}
-      onRedirectCallback={onRedirectCallback}
+      onRedirectCallback={(appState: any) => {
+        window.location.replace(
+          appState && appState.targetUrl
+            ? appState.targetUrl
+            : window.location.pathname
+        );
+      }}
     >
     <App />
   </Auth0Provider>
