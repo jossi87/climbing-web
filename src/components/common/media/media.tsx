cd734ddaa62148e93c63972f63787a5cb9f48dff
Jostein Øygarden <jostein.oygarden@gmail.com>
2020-03-20 19:22:46 +0100

    fix

--- src/components/common/media/media.tsx
+++ src/components/common/media/media.tsx
@@ -84,6 +84,9 @@ const Media = ({ media, removeMedia, useBlueNotRed, isAdmin }) => {
   }
   if (history.location.search && media && m == null) {
     let id = history.location.search.replace("?idMedia=","");
+    if (id.indexOf("&") > 0) {
+      id = id.substr(id, id.indexOf("&"));
+    }
     let x = media.filter(m => m.id==id);
     if (x && x.length > 0) {
       setM(x[0]);
