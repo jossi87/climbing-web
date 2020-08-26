31b2e6554180a3f5b4d1c35ce070d21397f071c1
jostein <jostein.oygarden@verico.com>
2020-08-26 09:00:47 +0200

    leaflet-print

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Auth0Provider } from './utils/react-auth0-spa';
 import App from './app';
+import LeafletPrint from './leaflet-print';
 
 const Index = () => (
   <Auth0Provider
@@ -10,7 +11,12 @@ const Index = () => (
       client_id='DNJNVzhxbF7PtaBFh7H6iBSNLh2UJWHt'
       redirect_uri={window.location.origin}
     >
-    <Router><App /></Router>
+    <Router>
+      <Switch>
+        <Route exact path="/leaflet-print/:json" component={LeafletPrint} />
+        <Route component={App}/>
+      </Switch>
+    </Router>
   </Auth0Provider>
 );
 
---
+++ src/leaflet-print.tsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import Leaflet from './components/common/leaflet/leaflet';
+
+interface LeafletPrintProps {
+  json: string;
+}
+const LeafletPrint = () => {
+  let { json } = useParams<LeafletPrintProps>();
+  let leaflet = JSON.parse(json);
+  const outlines = leaflet.outlines.map(o => {
+    const polygon = o.polygonCoords.split(";").map((c, i) => {
+      const latLng = c.split(",");
+      return ([parseFloat(latLng[0]), parseFloat(latLng[1])]);
+    });
+    return {label: o.name, polygon}
+  });
+  const polylines = leaflet.polylines.map(p => {
+    return {
+      label: p.name,
+      polyline: p.polyline.split(";").map(e => {
+        return e.split(",").map(Number);
+      })
+    }
+  });
+  return (
+    <Leaflet height={'100vh'} markers={leaflet.markers} outlines={outlines} polylines={polylines} defaultCenter={leaflet.defaultCenter} defaultZoom={leaflet.defaultZoom} history={null} onClick={null} />
+  );
+}
+
+export default LeafletPrint;
