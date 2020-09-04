f5e29537e5538c0117bdaaff55c828b647228308
jostein <jostein.oygarden@verico.com>
2020-09-04 16:00:46 +0200

    x

--- leaflet-puppeteer-print/index.js
+++ leaflet-puppeteer-print/index.js
@@ -74,7 +74,7 @@ function initMap(leaflet) {
     group.addTo(map);
     if (num > 1) {
       let bounds = group.getBounds();
-      map.fitBounds(bounds.pad(0.032));
+      map.fitBounds(bounds.pad(0.032), {maxZoom: 18});
     }
     if (leaflet.legends && leaflet.legends.length>0) {
       let legend = leaflet.legends.join('<br/>');
