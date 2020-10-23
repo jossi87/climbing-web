56050f494928d5f2444799173ea517d23d3dec64
Jostein Øygarden <jostein.oygarden@verico.com>
2020-10-23 09:43:03 +0200

    x

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ module.exports = {
   entry: [
     './src/index.tsx'
   ],
-  devtool: 'inline-source-map',
   module: {
     rules: [
       { test: /\.tsx?$/, exclude: /node_modules/, use: 'ts-loader' }
