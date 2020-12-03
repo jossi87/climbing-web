12c8c53d7fd106a00611346fa5f894ed021b472a
Jostein Øygarden <jostein.oygarden@verico.com>
2020-12-03 09:03:46 +0100

    x

--- src/components/problem-hse.tsx
+++ src/components/problem-hse.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import MetaTags from 'react-meta-tags';
 import { Link } from 'react-router-dom';
 import { LockSymbol, LoadingAndRestoreScroll } from './common/widgets/widgets';
-import { Table, Header } from 'semantic-ui-react';
+import { Segment, List, Header } from 'semantic-ui-react';
 import { useAuth0 } from '../utils/react-auth0-spa';
 import { getProblemHse } from '../api';
 
@@ -18,37 +18,31 @@ const ProblemHse = () => {
   if (!data) {
     return <LoadingAndRestoreScroll />;
   }
-  const rows = data.map((hse, i) => {
-    return (
-      <Table.Row key={i}>
-        <Table.Cell><Link to={`/area/${hse.areaId}`}>{hse.areaName}</Link> <LockSymbol lockedAdmin={hse.areaLockedAdmin} lockedSuperadmin={hse.areaLockedSuperadmin} /></Table.Cell>
-        <Table.Cell><Link to={`/sector/${hse.sectorId}`}>{hse.sectorName}</Link> <LockSymbol lockedAdmin={hse.sectorLockedAdmin} lockedSuperadmin={hse.sectorLockedSuperadmin} /></Table.Cell>
-        <Table.Cell><Link to={`/problem/${hse.problemId}`}>{hse.problemName}</Link> <LockSymbol lockedAdmin={hse.problemLockedAdmin} lockedSuperadmin={hse.problemLockedSuperadmin} /></Table.Cell>
-        <Table.Cell>{hse.comment}</Table.Cell>
-      </Table.Row>
-    )
-  });
-
   return (
     <>
       <MetaTags>
         <title>Flagged as dangerous</title>
         <meta name="description" content={"HSE"} />
       </MetaTags>
-      <Header>Health and Safety Executive (HSE)</Header>
-      <Table celled compact unstackable>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Area</Table.HeaderCell>
-            <Table.HeaderCell>Sector</Table.HeaderCell>
-            <Table.HeaderCell>Problem</Table.HeaderCell>
-            <Table.HeaderCell>Comment</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {rows}
-        </Table.Body>
-      </Table>
+      <Segment>
+        <Header>Health and Safety Executive (HSE)</Header>
+        <List divided relaxed>
+          {data.map((hse, i) => (
+            <List.Item key={i}>
+              <List.Content>
+                <List.Header>
+                  <Link to={`/area/${hse.areaId}`}>{hse.areaName}</Link> <LockSymbol lockedAdmin={hse.areaLockedAdmin} lockedSuperadmin={hse.areaLockedSuperadmin} />
+                  {' / '}
+                  <Link to={`/sector/${hse.sectorId}`}>{hse.sectorName}</Link> <LockSymbol lockedAdmin={hse.sectorLockedAdmin} lockedSuperadmin={hse.sectorLockedSuperadmin} />
+                  {' / '}
+                  <Link to={`/problem/${hse.problemId}`}>{hse.problemName}</Link> <LockSymbol lockedAdmin={hse.problemLockedAdmin} lockedSuperadmin={hse.problemLockedSuperadmin} />
+                </List.Header>
+                <List.Description>{hse.comment}</List.Description>
+              </List.Content>
+            </List.Item>
+          ))}
+        </List>
+      </Segment>
     </>
   );
 }
