38e96d0d8eb38bf01b30d88d369c4650a1a3572c
Jostein Øygarden <jostein.oygarden@verico.com>
2021-04-20 13:24:07 +0200

    tooltip

--- src/components/common/chart-grade-distribution/chart-grade-distribution.tsx
+++ src/components/common/chart-grade-distribution/chart-grade-distribution.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { LoadingAndRestoreScroll } from '../widgets/widgets';
+import { Popup } from 'semantic-ui-react'
 import { getGradeDistribution } from './../../../api';
 
 const ChartGradeDistribution = ({accessToken, idArea, idSector}) => {
@@ -18,13 +19,26 @@ const ChartGradeDistribution = ({accessToken, idArea, idSector}) => {
   const cols = gradeDistribution.map((g, i) => {
     const hPrim = (g.prim/maxValue*80) + '%';
     const hSec = (g.sec/maxValue*80) + '%';
-    return (
-        <td key={i} style={{height: '100%', verticalAlign: 'bottom', textAlign: 'center'}}>
-          {g.num>0 && g.num}
-          {g.sec>0 && <div style={{marginLeft: '3px', marginRight: '3px', height: hSec, backgroundColor: '#BD313C'}} />}
-          {g.prim>0 && <div style={{marginLeft: '3px', marginRight: '3px', paddingBottom: hSec, height: hPrim, backgroundColor: '#3182bd'}} />}
-        </td>
-    )
+    const col = (
+      <td key={i} style={{height: '100%', verticalAlign: 'bottom', textAlign: 'center'}}>
+        {g.num>0 && g.num}
+        {g.sec>0 && <div style={{marginLeft: '3px', marginRight: '3px', height: hSec, backgroundColor: '#BD313C'}} />}
+        {g.prim>0 && <div style={{marginLeft: '3px', marginRight: '3px', paddingBottom: hSec, height: hPrim, backgroundColor: '#3182bd'}} />}
+      </td>
+    );
+    if (g.tooltip) {
+      return (
+          <Popup
+            position="bottom center"
+            inverted
+            offset={[0, 20]}
+            trigger={col}
+            content={g.tooltip}
+            size='mini'
+          />
+      );
+    }
+    return col;
   });
   return (
     <table style={{height: '20vh', tableLayout: 'fixed', width: '100%', maxWidth: '400px'}}>
