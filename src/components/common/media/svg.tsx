f0d70b5cfe4a6f1bb7b7e92d7d712ab61fc42129
Jostein Øygarden <jostein.oygarden@verico.com>
2021-06-07 16:06:41 +0200

    x

--- src/components/common/media/svg.tsx
+++ src/components/common/media/svg.tsx
@@ -114,7 +114,7 @@ const Svg = ({ style, close, m, thumb, optProblemId }) => {
   }
   
   let info;
-  if (!thumb && optProblemId && optProblemId>0 && m.svgs.filter(x => x.problemId===optProblemId).length===1) {
+  if (!thumb && optProblemId && optProblemId>0 && m.svgs && m.svgs.filter(x => x.problemId===optProblemId).length===1) {
     let svg = m.svgs.filter(x => x.problemId===optProblemId)[0];
     let text = `#${svg.nr} - ${svg.problemName} [${svg.problemGrade}]`;
     if (svg.problemSubtype) {
