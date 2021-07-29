13ece6471a3bcbc94a306b1330be5ec7d0eb34ed
Jostein Øygarden <jostein.oygarden@verico.com>
2021-07-29 12:48:05 +0200

    screenfull not working in safari on iPhone, reverted to old version of leaflet.fullscreen

--- src/components/common/leaflet/fullscreencontrol.tsx
+++ src/components/common/leaflet/fullscreencontrol.tsx
@@ -1,16 +1,13 @@
 import { useEffect } from "react";
 import { useMap } from "react-leaflet";
-import screenfull from 'screenfull';
-import 'leaflet.fullscreen';
+import "leaflet.fullscreen";
 
 export default function FullscreenControl() {
-  window.screenfull = screenfull; // Necessary to get leaflet.fullscreen 2.0.0 to work with react-leaflet 3.0.0 (https://github.com/brunob/leaflet.fullscreen/issues/81)
   const map = useMap();
   useEffect(() => {
     const L = require('leaflet')
     const fullscreen = L.control.fullscreen();
     fullscreen.addTo(map);
   }, [map]);
-
   return null;
 }
