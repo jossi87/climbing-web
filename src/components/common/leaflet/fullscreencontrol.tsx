c45a8c0187c9393f656649c295c6bb3ab5ef01a8
Jostein Øygarden <jostein.oygarden@verico.com>
2021-11-16 08:13:45 +0100

    third party libraries

--- src/components/common/leaflet/fullscreencontrol.tsx
+++ src/components/common/leaflet/fullscreencontrol.tsx
@@ -2,8 +2,6 @@ import { useEffect } from "react";
 import { useMap } from "react-leaflet";
 import "leaflet.fullscreen";
 
-// leaflet.fullscreen 2.0.0 does not work: https://github.com/brunob/leaflet.fullscreen/issues/81
-// The quick fix to include screenfull crashes on iPhone (screenfull not supported on Safari): Error message: undefined is not an object (evaluating 'screenfull.raw.fullscreenchange') [mail from palgra@yahoo.com 2021.07.29 - 12:25]
 export default function FullscreenControl() {
   const map = useMap();
   useEffect(() => {
