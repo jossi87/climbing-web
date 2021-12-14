24f7643e8ba1623515570c9e61cc1041f48d8f98
Jostein Øygarden <jostein.oygarden@verico.com>
2021-12-14 14:27:37 +0100

    third party libs

--- src/components/common/profile/profile-todo.tsx
+++ src/components/common/profile/profile-todo.tsx
@@ -52,16 +52,16 @@ const ProfileTodo = ({accessToken, userId, defaultCenter, defaultZoom}) => {
         <List celled>
           {data.areas.map((area, i) => (
             <List.Item key={i}>
-              <List.Header><Link id={area.id} to={{pathname: area.url}} target='_blank'>{area.name}</Link><LockSymbol lockedAdmin={area.lockedAdmin} lockedSuperadmin={area.lockedSuperadmin} /></List.Header>
+              <List.Header><a href={area.url} target='_blank'>{area.name}</a><LockSymbol lockedAdmin={area.lockedAdmin} lockedSuperadmin={area.lockedSuperadmin} /></List.Header>
               {area.sectors.map((sector, i) => (
                 <List.List key={i}>
-                  <List.Header><Link to={{pathname: sector.url}} target='_blank'>{sector.name}</Link><LockSymbol lockedAdmin={sector.lockedAdmin} lockedSuperadmin={sector.lockedSuperadmin} /></List.Header>
+                  <List.Header><a href={sector.url} target='_blank'>{sector.name}</a><LockSymbol lockedAdmin={sector.lockedAdmin} lockedSuperadmin={sector.lockedSuperadmin} /></List.Header>
                   <List.List>
                     {sector.problems.map((problem, i) => (
                       <List.Item key={i}>
                         <List.Header>
                           {`#${problem.nr} `}
-                          <Link to={{pathname: problem.url}} target='_blank'>{problem.name}</Link>
+                          <a href={problem.url} target='_blank'>{problem.name}</a>
                           {' '}{problem.grade}
                           {problem.partners && problem.partners.length>0 &&
                             <small>
