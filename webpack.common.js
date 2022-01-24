4d73a326138c08ffc2215dc1ebb5c462fe77e2b7
Jostein Øygarden <jostein.oygarden@verico.com>
2022-01-24 08:48:03 +0100

    third party libs

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,6 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin').default;
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: [
