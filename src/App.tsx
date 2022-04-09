9f9cda3f47395b8e992fb846bc31408bef85f5c0
Jostein Øygarden <jostein.oygarden@gmail.com>
2022-04-09 09:42:03 +0200

    scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { Container } from "semantic-ui-react"
-import Header from "./components/Header"
+import { Container } from "semantic-ui-react";
+import Header from "./components/Header";
 import AppRoutes from './components/AppRoutes';
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 
 const App = () => {
   
--- src/utils/scroll-to-top.tsx
+++ src/utils/scroll-to-top.tsx
@@ -4,7 +4,6 @@ import { useLocation } from "react-router";
 const ScrollToTop = (props) => {
   const location = useLocation();
   useEffect(() => {
-    console.log(location)
     window.scrollTo(0, 0);
   }, [location]);
 
