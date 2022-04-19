b757ef80ee02c766d2e8bdf54de9091c86717d32
Jostein Øygarden <jostein.oygarden@verico.com>
2022-04-19 10:47:47 +0200

    x

--- src/components/common/leaflet/fullscreencontrol.tsx
+++ src/components/common/leaflet/fullscreencontrol.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useMap } from "react-leaflet";
 import "leaflet.fullscreen";
 
+// Version 2.3.0 does not work in iPhone: https://github.com/brunob/leaflet.fullscreen/issues/99 - yields "undefined is not an object (evaluating 'this._screenfull.raw.fullscreenchange')". Keep v1.6.0
 export default function FullscreenControl() {
   const map = useMap();
   useEffect(() => {
