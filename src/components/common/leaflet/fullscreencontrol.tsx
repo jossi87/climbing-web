ddc0fd93d559f17890258d9a45ec55116dad7dca
Jostein Øygarden <jostein.oygarden@verico.com>
2022-04-19 10:12:51 +0200

    leaflet.fullscreen finally updated

--- src/components/common/leaflet/fullscreencontrol.tsx
+++ src/components/common/leaflet/fullscreencontrol.tsx
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import { useMap } from "react-leaflet";
 import "leaflet.fullscreen";
 
-// https://github.com/brunob/leaflet.fullscreen/issues/92 <--Use leaflet.fullscreen 1.6.0, newer will not work on Safari
 export default function FullscreenControl() {
   const map = useMap();
   useEffect(() => {
