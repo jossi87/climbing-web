e9479dbec3874641b75a3f23b6b579186523ebe8
Jostein Øygarden <jostein.oygarden@verico.com>
2022-09-14 08:59:07 +0200

    third party libs

--- src/index.tsx
+++ src/index.tsx
@@ -18,32 +18,13 @@ const analytics = Analytics({
   ]
 })
 
-function getBrowser() {
-  var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []; 
-  if(/trident/i.test(M[1])){
-      tem=/\brv[ :]+(\d+)/g.exec(ua) || []; 
-      return {name:'IE',version:(tem[1]||'')};
-      }   
-  if(M[1]==='Chrome'){
-      tem=ua.match(/\bOPR|Edge\/(\d+)/)
-      if(tem!=null)   {return {name:'Opera', version:tem[1]};}
-      }   
-  M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
-  if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
-  return {
-    name: M[0],
-    version: M[1]
-  };
-}
-
 function ErrorFallback({error, resetErrorBoundary}) {
-  let browser = getBrowser();
-  let browserStr = browser? browser.name + " " + browser.version : "<null>";
+  const userAgent = navigator.userAgent;
   return (
     <div role="alert">
       <h1>Something went wrong</h1>
-      <b>Browser:</b>
-      <pre>{browserStr}</pre>
+      <b>User Agent:</b>
+      <pre>{userAgent}</pre>
       <b>Error message:</b>
       <pre>{error.message}</pre>
       <b>Stack trace:</b>
@@ -51,7 +32,7 @@ function ErrorFallback({error, resetErrorBoundary}) {
       <button onClick={resetErrorBoundary}>Try again</button>
       <button onClick={() => {
         let body = "URL: " + window.location.href + "%0D%0A%0D%0A"
-                + "Browser: " + browserStr + "%0D%0A%0D%0A"
+                + "User Agent: " + userAgent + "%0D%0A%0D%0A"
                 + "Error message: " + error.message + "%0D%0A%0D%0A"
                 + "Stack trace:%0D%0A" + error.stack;
         let link = "mailto:jostein.oygarden@gmail.com"
