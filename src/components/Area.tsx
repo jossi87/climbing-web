e2cc5e46398b52bcb846e856d206f6e0cbbcea26
Jostein Øygarden <jostein.oygarden@esmartsystems.com>
2023-03-10 16:01:46 +0100

    x

--- src/components/Area.tsx
+++ src/components/Area.tsx
@@ -150,9 +150,12 @@ const Area = () => {
             <Table.Row negative verticalAlign="top">
               <Table.Cell>Area restrictions:</Table.Cell>
               <Table.Cell>
-                <Header as="h5" color="red">
-                  <Image src="/svg/no-animals.svg" alt="No dogs allowed"/><br/>
-                  The access to our crags are at the mercy of the farmers who own the land. Because of conflicts between dog-owners and farmers (loose dogs scaring & killing the farmers animals) we ask you to not bring your dog to this spesific crag, thank you.
+                <Header as="h5" color="red" image>
+                  <Image src="/svg/no-animals.svg" alt="No dogs allowed" rounded size='mini'/>
+                  <Header.Content>
+                    The access to our crags are at the mercy of the farmers who own the land.
+                    <Header.Subheader>Because of conflicts between dog-owners and farmers we ask you to not bring your dog to this spesific crag.</Header.Subheader>
+                  </Header.Content>
                 </Header>
               </Table.Cell>
             </Table.Row>
