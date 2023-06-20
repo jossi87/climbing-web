d8ed61888e1101644b40de5178b6984d2aa68c8f
Jostein Øygarden <jostein.oygarden@esmartsystems.com>
2023-06-20 14:38:41 +0200

    swagger

---
+++ src/components/Swagger.tsx
@@ -0,0 +1,9 @@
+import { Segment } from "semantic-ui-react";
+import SwaggerUI from "swagger-ui-react";
+import "swagger-ui-react/swagger-ui.css";
+
+const Swagger = () => {
+  return <Segment><SwaggerUI url="https://brattelinjer.no/com.buldreinfo.jersey.jaxb/swagger.json" /></Segment>
+}
+  
+export default Swagger;
