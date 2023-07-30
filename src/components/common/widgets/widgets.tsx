ed41421d9be0617d0e83385e3fed98b5324ba9c7
Evan Charlton <evancharlton@gmail.com>
2023-07-30 10:53:35 +0200

    Show the real weather + forecast for areas/sectors
    Instead of only giving a link to the weather (and assuming rain), show
    the predicted realtime data from Yr for the given coordinates.

--- src/components/common/widgets/widgets.tsx
+++ src/components/common/widgets/widgets.tsx
@@ -1,4 +1,5 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import {
   Segment,
@@ -10,6 +11,7 @@ import {
   Button,
 } from "semantic-ui-react";
 import SunCalc from "suncalc";
+import { TWeatherSymbolKey, weatherSymbolKeys } from "../../../yr";
 
 type LockSymbolProps = {
   lockedAdmin: boolean;
@@ -176,50 +178,152 @@ type WeatherLabelsProps = {
   label: string;
 };
 
-export function WeatherLabels({ lat, lng, label }: WeatherLabelsProps) {
-  if (lat && lng) {
-    const times = SunCalc.getTimes(new Date(), lat, lng);
-    return (
-      <>
-        <Label
-          href={`https://www.yr.no/en/forecast/daily-table/${lat},${lng}`}
-          rel="noopener"
-          target="_blank"
-          image
-          basic
-        >
-          <Icon name="rain" />
-          Yr.no
-        </Label>
-        <Label
-          href={`/webcams/` + JSON.stringify({ lat, lng, label })}
-          rel="noopener"
-          target="_blank"
-          image
-          basic
-        >
-          <Icon name="camera" />
-          Webcams
-        </Label>
-        {times.sunrise != "Invalid Date" && (
-          <Popup
-            content="Sunrise and sunset"
-            trigger={
-              <Label basic>
-                <Icon name="sun" />
-                {String(times.sunrise.getHours()).padStart(2, "0") +
-                  ":" +
-                  String(times.sunrise.getMinutes()).padStart(2, "0") +
-                  " - " +
-                  String(times.sunset.getHours()).padStart(2, "0") +
-                  ":" +
-                  String(times.sunset.getMinutes()).padStart(2, "0")}
-              </Label>
-            }
-          />
-        )}
-      </>
-    );
+type YrData = {
+  properties: {
+    timeseries: {
+      time: string;
+      data: {
+        next_1_hours: {
+          summary: {
+            symbol_code: TWeatherSymbolKey;
+          };
+        };
+        next_6_hours: {
+          summary: {
+            symbol_code: TWeatherSymbolKey;
+          };
+        };
+        next_12_hours: {
+          summary: {
+            symbol_code: TWeatherSymbolKey;
+          };
+        };
+      };
+    }[];
+  };
+};
+
+const WeatherIcon = ({
+  symbol,
+}: {
+  symbol: undefined | "loading" | TWeatherSymbolKey;
+}) => {
+  if (symbol === "loading") {
+    return <Icon name="spinner" loading />;
   }
-  return null;
+
+  if (symbol && weatherSymbolKeys[symbol]) {
+    return <img src={`/svg/yr/${weatherSymbolKeys[symbol]}.svg`} />;
+  }
+
+  return <Icon name="rain" />;
+};
+
+const YrLink = ({ lat, lng }: Pick<WeatherLabelsProps, "lat" | "lng">) => {
+  const { data: weatherData, isLoading } = useQuery(
+    [`yr/weather`, { lat, lng }],
+    {
+      enabled: !!lat && !!lng,
+      queryFn: () =>
+        fetch(
+          `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${lat}&lon=${lng}`,
+        ).then((res) => res.json() as Promise<YrData>),
+      select: (data) => data.properties?.timeseries?.[0]?.data,
+    },
+  );
+
+  const next1Hours = weatherData?.next_1_hours?.summary?.symbol_code;
+  const next6Hours = weatherData?.next_6_hours?.summary?.symbol_code;
+  const next12Hours = weatherData?.next_12_hours?.summary?.symbol_code;
+
+  const label = (
+    <Label
+      href={`https://www.yr.no/en/forecast/daily-table/${lat},${lng}`}
+      rel="noopener"
+      target="_blank"
+      image
+      basic
+    >
+      <WeatherIcon symbol={isLoading ? "loading" : next1Hours} />
+      Yr.no
+    </Label>
+  );
+
+  if (isLoading || !weatherData) {
+    return label;
+  }
+
+  return (
+    <Popup trigger={label}>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                width={50}
+                src={`/svg/yr/${weatherSymbolKeys[next1Hours]}.svg`}
+              />
+            </td>
+            <td>
+              <img
+                width={50}
+                src={`/svg/yr/${weatherSymbolKeys[next6Hours]}.svg`}
+              />
+            </td>
+            <td>
+              <img
+                width={50}
+                src={`/svg/yr/${weatherSymbolKeys[next12Hours]}.svg`}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td style={{ textAlign: "center" }}>1 hr</td>
+            <td style={{ textAlign: "center" }}>6 hrs</td>
+            <td style={{ textAlign: "center" }}>12 hrs</td>
+          </tr>
+        </tbody>
+      </table>
+    </Popup>
+  );
+};
+
+export function WeatherLabels({ lat, lng, label }: WeatherLabelsProps) {
+  if (!lat || !lng) {
+    return;
+  }
+
+  const times = SunCalc.getTimes(new Date(), lat, lng);
+  return (
+    <>
+      <YrLink lat={lat} lng={lng} />
+      <Label
+        href={`/webcams/` + JSON.stringify({ lat, lng, label })}
+        rel="noopener"
+        target="_blank"
+        image
+        basic
+      >
+        <Icon name="camera" />
+        Webcams
+      </Label>
+      {times.sunrise != "Invalid Date" && (
+        <Popup
+          content="Sunrise and sunset"
+          trigger={
+            <Label basic>
+              <Icon name="sun" />
+              {String(times.sunrise.getHours()).padStart(2, "0") +
+                ":" +
+                String(times.sunrise.getMinutes()).padStart(2, "0") +
+                " - " +
+                String(times.sunset.getHours()).padStart(2, "0") +
+                ":" +
+                String(times.sunset.getMinutes()).padStart(2, "0")}
+            </Label>
+          }
+        />
+      )}
+    </>
+  );
 }
