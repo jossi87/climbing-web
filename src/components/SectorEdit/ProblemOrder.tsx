edbeb8edb844d33af4433d0ffe3806cc2cafe28e
Evan Charlton <evancharlton@gmail.com>
2023-08-29 17:18:01 +0200

    Refactor the problem reordering feature
    The `SectorEdit` page has a component to allow the problems for a sector
    to be presented in a different order. This was implemented in a way that
    polluted the state with unexpected props (it just made use of
    JavaScript's type flexibility).
    
    This makes it a bit annoying to type the rest of the component, so this
    change pulls it out to a separate component and reworks the logic a
    little bit to add type safety.

---
+++ src/components/SectorEdit/ProblemOrder.tsx
@@ -0,0 +1,50 @@
+import { useRef } from "react";
+import { definitions } from "../../@types/buldreinfo/swagger";
+import { Input } from "semantic-ui-react";
+
+type Props = Pick<definitions["Sector"], "problemOrder"> & {
+  onChange: (order: NonNullable<definitions["Sector"]["problemOrder"]>) => void;
+};
+
+export const ProblemOrder = ({ problemOrder, onChange }: Props) => {
+  const originalOrder = useRef(
+    (problemOrder ?? []).reduce(
+      (acc, { id, nr }) => ({ ...acc, [id]: nr }),
+      {},
+    ),
+  ).current;
+
+  return problemOrder.map(({ id, name, nr }) => {
+    const isModified = nr !== originalOrder[id];
+    const color = isModified ? "orange" : "grey";
+    return (
+      <Input
+        key={id}
+        size="small"
+        type="number"
+        step={1}
+        fluid
+        icon="hashtag"
+        iconPosition="left"
+        placeholder="Number"
+        defaultValue={nr}
+        label={{ basic: true, content: name, color }}
+        labelPosition="right"
+        onChange={(_, { value }) => {
+          const num = +value;
+          onChange(
+            problemOrder.map((problem) => {
+              if (problem.id === id) {
+                return {
+                  ...problem,
+                  nr: num,
+                };
+              }
+              return problem;
+            }),
+          );
+        }}
+      />
+    );
+  });
+};
