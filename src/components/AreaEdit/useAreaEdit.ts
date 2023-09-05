0b3a23ff2b44aed7903a93c43fbb41fdd99c87f0
Jostein Øygarden <jostein.oygarden@esmartsystems.com>
2023-09-05 07:19:53 +0200

    const instead of let

--- src/components/AreaEdit/useAreaEdit.ts
+++ src/components/AreaEdit/useAreaEdit.ts
@@ -117,7 +117,7 @@ const reducer = (state: State, update: Update): State => {
     }
     case "set-coord": {
       const { key, value } = update;
-      let coordinates = state.coordinates || { latitude: 0, longitude: 0 };
+      const coordinates = state.coordinates || { latitude: 0, longitude: 0 };
       coordinates[key] = getCoord(value);
       return { ...state, coordinates };
     }
