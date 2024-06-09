4321c0f84c3e6b55f8b7bfaaab72b9073163f7c3
Evan Charlton <evancharlton@gmail.com>
2024-06-09 06:32:32 +0200

    chore: Re-add react & react-hooks ESLint rules
    During the eslint upgrade, the `react` and `react-hooks` ESLint plugins
    were dropped because they didn't support the new ESLint config format.
    They still don't support the new format (yet) but this uses the
    `@eslint/compat` package to bring them in.

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,35 @@
 const eslint = require("@eslint/js");
-const tseslint = require ("typescript-eslint");
+const tseslint = require("typescript-eslint");
+const react = require("eslint-plugin-react");
+const reactHooks = require("eslint-plugin-react-hooks");
+const { fixupPluginRules } = require("@eslint/compat");
 
 module.exports = [
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    plugins: {
+      react: fixupPluginRules(react),
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    plugins: {
+      "react-hooks": fixupPluginRules(reactHooks),
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-hooks/exhaustive-deps": "error",
+    },
+  },
   {
     files: ["src/**/*.{ts,tsx}"],
     rules: {
@@ -17,6 +43,8 @@ module.exports = [
         },
       ],
     },
+  },
+  {
     ignores: ["webpack.*", "swagger.d.ts"],
-  }
-]
+  },
+];
