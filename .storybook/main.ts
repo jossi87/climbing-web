jossi87/climbing-web

commit 76e135f3650b1afff3a8273a7bc60f63224e6d44

chore: Fix more TypeScript errors
The march towards `strict: true` continues with fixing another 100
violations.

More to come!

src/components/common/widgets/widgets.tsx
@@ -176,8 +176,10 @@ type ConditionLabelsProps = {
   lat: number;
   lng: number;
   label: string;
-  wallDirectionCalculated: components["schemas"]["CompassDirection"];
-  wallDirectionManual: components["schemas"]["CompassDirection"];
+  wallDirectionCalculated:
+    | components["schemas"]["CompassDirection"]
+    | undefined;
+  wallDirectionManual: components["schemas"]["CompassDirection"] | undefined;
   sunFromHour: number;
   sunToHour: number;
 };
@@ -200,9 +202,8 @@ export const WallDirection = ({
         trigger={
           <Label basic size="small">
             <Icon name="compass outline" />
-            {wallDirectionManual
-              ? wallDirectionManual.direction
-              : wallDirectionCalculated.direction}
+            {wallDirectionManual?.direction ??
+              wallDirectionCalculated?.direction}
           </Label>
         }
       />

src/api/operations.ts
@@ -345,7 +345,7 @@ export function postTicks(
   id: number,
   idProblem: number,
   comment: string,
-  date: string,
+  date: string | undefined,
   stars: number,
   grade: string,
   repeats: any,

src/api/utils.ts
@@ -95,7 +95,7 @@ export function numberWithCommas(number: number) {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-export function convertFromDateToString(date: Date): string | null {
+export function convertFromDateToString(date: Date | null): string | null {
   if (!date) {
     return null;
   }

src/components/Problem/Problem.tsx
@@ -37,6 +37,10 @@ import { DownloadButton } from "../common/DownloadButton";
 
 export const Problem = () => {
   const { problemId } = useParams();
+  if (!problemId) {
+    throw new Error("Missing problemId from params");
+  }
+
   const [showHiddenMedia, setShowHiddenMedia] = useState(false);
   const meta = useMeta();
   const { data, error, toggleTodo, redirectUi } = useProblem(
@@ -79,23 +83,23 @@ export const Problem = () => {
     return <Loading />;
   }
 
-  const orderableMedia = [];
-  const carouselMedia = [];
-  if (data.media?.length > 0) {
+  const orderableMedia: ComponentProps<typeof Media>["orderableMedia"] = [];
+  const carouselMedia: ComponentProps<typeof Media>["carouselMedia"] = [];
+  if (data.media?.length) {
     carouselMedia.push(...data.media);
     if (data.media.length > 1) {
       orderableMedia.push(...data.media);
     }
   }
-  if (data.triviaMedia?.length > 0) {
+  if (data.triviaMedia?.length) {
     carouselMedia.push(...data.triviaMedia);
     if (data.triviaMedia.length > 1) {
       orderableMedia.push(...data.triviaMedia);
     }
   }
-  if (data.sections?.length > 0) {
+  if (data.sections?.length) {
     data.sections.forEach((s) => {
-      if (s.media?.length > 0) {
+      if (s.media?.length) {
         carouselMedia.push(...s.media);
         if (s.media.length > 1) {
           orderableMedia.push(...s.media);
@@ -103,9 +107,9 @@ export const Problem = () => {
       }
     });
   }
-  if (data.comments?.length > 0) {
+  if (data.comments?.length) {
     data.comments.forEach((c) => {
-      if (c.media?.length > 0) {
+      if (c.media?.length) {
         carouselMedia.push(...c.media);
         if (c.media.length > 1) {
           orderableMedia.push(...c.media);
@@ -130,16 +134,17 @@ export const Problem = () => {
   }
   const panes: ComponentProps<typeof Tab>["panes"] = [];
   if (data.media && data.media.length > 0) {
+    const media = data.media;
     panes.push({
       menuItem: { key: "media", icon: "image" },
       render: () => (
         <Tab.Pane>
           <Media
             numPitches={data.sections?.length || 0}
-            media={data.media}
+            media={media}
             orderableMedia={orderableMedia}
             carouselMedia={carouselMedia}
-            optProblemId={data.id}
+            optProblemId={data.id ?? 0}
             showLocation={false}
           />
         </Tab.Pane>
@@ -147,19 +152,19 @@ export const Problem = () => {
     });
   }
   if (markers.length > 0) {
-    let outlines;
-    let approaches;
-    if (data.sectorOutline?.length > 0 && !data.coordinates) {
+    let outlines: ComponentProps<typeof Leaflet>["outlines"];
+    let approaches: ComponentProps<typeof Leaflet>["approaches"];
+    if (data.sectorOutline?.length && !data.coordinates) {
       const outline = data.sectorOutline;
       outlines = [
         { url: "/sector/" + data.sectorId, label: data.sectorName, outline },
       ];
     }
-    if (data.sectorApproach?.coordinates?.length > 0) {
+    if (data.sectorApproach?.coordinates?.length) {
       approaches = [
         {
           approach: data.sectorApproach,
-          label: getDistanceWithUnit(data.sectorApproach),
+          label: getDistanceWithUnit(data.sectorApproach) ?? undefined,
         },
       ];
     }
@@ -174,10 +179,15 @@ export const Problem = () => {
             markers={markers}
             outlines={outlines}
             approaches={approaches}
-            defaultCenter={{
-              lat: markers[0].coordinates.latitude,
-              lng: markers[0].coordinates.longitude,
-            }}
+            defaultCenter={
+              markers[0].coordinates.latitude &&
+              markers[0].coordinates.longitude
+                ? {
+                    lat: markers[0].coordinates.latitude,
+                    lng: markers[0].coordinates.longitude,
+                  }
+                : meta.defaultCenter
+            }
             defaultZoom={16}
             showSatelliteImage={true}
             clusterMarkers={false}
@@ -192,26 +202,26 @@ export const Problem = () => {
     if (!showTickModal) {
       return null;
     }
-    const enableTickRepeats = meta.isIce || data.sections?.length > 0;
+    const enableTickRepeats = meta.isIce || data.sections?.length;
     const userTicks = data.ticks?.filter((t) => t.writable);
-    if (userTicks && userTicks.length > 0) {
+    if (userTicks && userTicks.length && userTicks[0].date) {
       return (
         <TickModal
-          idTick={userTicks[0].id}
+          idTick={userTicks[0].id ?? 0}
           idProblem={data.id}
           date={userTicks[0].date}
-          comment={userTicks[0].comment}
+          comment={userTicks[0].comment ?? ""}
           grade={
             userTicks[0].noPersonalGrade
               ? "No personal grade"
-              : userTicks[0].suggestedGrade
+              : userTicks[0].suggestedGrade ?? "No personal grade"
           }
           grades={meta.grades}
-          stars={userTicks[0].stars}
+          stars={userTicks[0].stars ?? 0}
           repeats={userTicks[0].repeats}
           open={showTickModal}
           onClose={onTickModalClose}
-          enableTickRepeats={enableTickRepeats}
+          enableTickRepeats={!!enableTickRepeats}
         />
       );
     }
@@ -219,43 +229,59 @@ export const Problem = () => {
       <TickModal
         idTick={-1}
         idProblem={data.id}
-        grade={data.originalGrade}
+        grade={data.originalGrade ?? meta.grades[0].grade}
         grades={meta.grades}
         open={showTickModal}
         onClose={onTickModalClose}
-        comment={null}
-        stars={null}
+        comment={""}
+        stars={0}
         repeats={undefined}
-        date={null}
-        enableTickRepeats={enableTickRepeats}
+        date={undefined}
+        enableTickRepeats={!!enableTickRepeats}
       />
     );
   })();
 
   const [conditionLat, conditionLng] = (() => {
-    if (data.coordinates) {
+    if (data.coordinates?.latitude && data.coordinates?.longitude) {
       return [+data.coordinates.latitude, +data.coordinates.longitude];
-    } else if (data.sectorOutline?.length > 0) {
+    }
+
+    if (data.sectorOutline?.length) {
       const center = GetCenterFromDegrees(
-        data.sectorOutline.map((c) => [c.latitude, c.longitude]),
+        data.sectorOutline
+          .filter(
+            (c): c is Required<Pick<typeof c, "latitude" | "longitude">> =>
+              !!(c.latitude && c.longitude),
+          )
+          .map((c) => [c.latitude, c.longitude]),
       );
-      return [+center[0], +center[1]];
-    } else if (data.sectorParking) {
+      if (center) {
+        return [+center[0], +center[1]];
+      }
+    }
+
+    if (data.sectorParking?.latitude && data.sectorParking?.longitude) {
       return [+data.sectorParking.latitude, +data.sectorParking.longitude];
     }
+
+    if (meta.defaultCenter.lat && meta.defaultCenter.lng) {
+      return [meta.defaultCenter.lat, meta.defaultCenter.lng];
+    }
+
     return [0, 0];
   })();
 
   let ticksComments;
-  if (data.ticks?.length > 0 && data.comments?.length > 0) {
+  if (data.ticks?.length && data.comments?.length) {
     ticksComments = (
       <Grid>
-        {data.ticks?.length > 0 && (
+        {data.ticks?.length && (
           <Grid.Column mobile={16} tablet={8} computer={8}>
             <ProblemTicks ticks={data.ticks} />
           </Grid.Column>
         )}
-        {data.comments?.length > 0 && (
+        {data.comments?.length && (
           <Grid.Column mobile={16} tablet={8} computer={8}>
             <ProblemComments
               onShowCommentModal={setShowCommentModal}
@@ -266,9 +292,9 @@ export const Problem = () => {
         )}
       </Grid>
     );
-  } else if (data.ticks?.length > 0) {
+  } else if (data.ticks?.length) {
     ticksComments = <ProblemTicks ticks={data.ticks} />;
-  } else if (data.comments?.length > 0) {
+  } else if (data.comments?.length) {
     ticksComments = (
       <ProblemComments
         onShowCommentModal={setShowCommentModal}
@@ -389,16 +415,16 @@ export const Problem = () => {
           <Breadcrumb.Section>
             <Link to={`/area/${data.areaId}`}>{data.areaName}</Link>{" "}
             <LockSymbol
-              lockedAdmin={data.areaLockedAdmin}
-              lockedSuperadmin={data.areaLockedSuperadmin}
+              lockedAdmin={!!data.areaLockedAdmin}
+              lockedSuperadmin={!!data.areaLockedSuperadmin}
             />
           </Breadcrumb.Section>
           <Breadcrumb.Divider icon="right angle" />
           <Breadcrumb.Section>
             <Link to={`/sector/${data.sectorId}`}>{data.sectorName}</Link>{" "}
             <LockSymbol
-              lockedAdmin={data.sectorLockedAdmin}
-              lockedSuperadmin={data.sectorLockedSuperadmin}
+              lockedAdmin={!!data.sectorLockedAdmin}
+              lockedSuperadmin={!!data.sectorLockedSuperadmin}
             />
           </Breadcrumb.Section>
           <Breadcrumb.Divider icon="right angle" />
@@ -406,8 +432,8 @@ export const Problem = () => {
             <span style={{ fontWeight: "normal" }}>#{data.nr}</span> {data.name}{" "}
             <span style={{ fontWeight: "normal" }}>{data.grade}</span>{" "}
             <LockSymbol
-              lockedAdmin={data.lockedAdmin}
-              lockedSuperadmin={data.lockedSuperadmin}
+              lockedAdmin={!!data.lockedAdmin}
+              lockedSuperadmin={!!data.lockedSuperadmin}
             />
           </Breadcrumb.Section>
         </Breadcrumb>
@@ -432,7 +458,7 @@ export const Problem = () => {
           }
         />
       )}
-      {panes?.length > 0 && <Tab panes={panes} />}
+      {panes?.length && <Tab panes={panes} />}
       <Table definition unstackable>
         <Table.Body>
           {(data.areaAccessInfo ||
@@ -548,7 +574,7 @@ export const Problem = () => {
               <Label basic>
                 Grade:<Label.Detail>{data.originalGrade}</Label.Detail>
               </Label>
-              {meta.isClimbing && (
+              {meta.isClimbing && data.t?.subType && (
                 <Label basic>
                   <Icon name="tag" />
                   {data.t.subType}
@@ -604,7 +630,7 @@ export const Problem = () => {
               )}
             </Table.Cell>
           </Table.Row>
-          {(data.trivia || data.triviaMedia?.length > 0) && (
+          {(data.trivia || data.triviaMedia?.length) && (
             <Table.Row verticalAlign="top">
               <Table.Cell>Trivia:</Table.Cell>
               <Table.Cell>
@@ -633,7 +659,7 @@ export const Problem = () => {
             problemId={+problemId}
             rock={data?.rock}
           />
-          {data.sectorApproach?.coordinates?.length > 1 && (
+          {(data.sectorApproach?.coordinates?.length ?? 0) > 1 && (
             <Table.Row verticalAlign="top">
               <Table.Cell>Approach:</Table.Cell>
               <Table.Cell>
@@ -668,11 +694,11 @@ export const Problem = () => {
                 <ConditionLabels
                   lat={conditionLat}
                   lng={conditionLng}
-                  label={data.name}
+                  label={data.name ?? ""}
                   wallDirectionCalculated={data.sectorWallDirectionCalculated}
                   wallDirectionManual={data.sectorWallDirectionManual}
-                  sunFromHour={data.areaSunFromHour}
-                  sunToHour={data.areaSunToHour}
+                  sunFromHour={data.areaSunFromHour ?? 0}
+                  sunToHour={data.areaSunToHour ?? 0}
                 />
               </Table.Cell>
             </Table.Row>

src/components/Problems/reducer.ts
@@ -788,10 +788,10 @@ export const useFilterState = (init?: Partial<UiState>) => {
     // state into separate reducers instead of one giant object.
     const inputState = Object.entries(state)
       .filter(([key]) => FILTER_INPUT_KEYS_SET.has(key))
-      .reduce<Partial<FilterInputs>>((acc, [k, v]) => {
-        acc[k] = v;
-        return acc;
-      }, {} satisfies Partial<FilterInputs>);
+      .reduce<Partial<FilterInputs>>(
+        (acc, [k, v]) => ({ ...acc, [k]: v }),
+        {} satisfies Partial<FilterInputs>,
+      );
 
     // We're trying to construct a minimal diff between the current filter and
     // the default filter. The simplest way to do this is by flattening the two

src/components/common/tick-modal/tick-modal.tsx
@@ -24,11 +24,11 @@ type TickModalProps = {
   idTick: number;
   idProblem: number;
   grades: components["schemas"]["Grade"][];
-  comment: string | null;
+  comment: string;
   grade: string;
-  stars: number | null;
+  stars: number;
   repeats: { date?: string; comment?: string }[] | undefined;
-  date: string | null;
+  date: string | undefined;
   enableTickRepeats: boolean;
 };
 
@@ -46,11 +46,13 @@ const TickModal = ({
   enableTickRepeats,
 }: TickModalProps) => {
   const accessToken = useAccessToken();
-  const [comment, setComment] = useState(initialComment);
+  const [comment, setComment] = useState(initialComment ?? "");
   const [grade, setGrade] = useState(initialGrade);
   const [stars, setStars] = useState(initialStars);
-  const [date, setDate] = useState(
-    idTick == -1 ? convertFromDateToString(new Date()) : initialDate,
+  const [date, setDate] = useState<string | undefined>(
+    idTick == -1
+      ? convertFromDateToString(new Date()) ?? initialDate
+      : initialDate,
   );
   const [repeats, setRepeats] = useState(initialRepeats);
   const today = new Date();
@@ -78,14 +80,20 @@ const TickModal = ({
                   showMonthDropdown
                   showYearDropdown
                   dropdownMode="select"
-                  selected={date && convertFromStringToDate(date)}
-                  onChange={(date) => setDate(convertFromDateToString(date))}
+                  selected={date ? convertFromStringToDate(date) : undefined}
+                  onChange={(date) => {
+                    const stringified = convertFromDateToString(date);
+                    if (!stringified) {
+                      return;
+                    }
+                    return setDate(stringified);
+                  }}
                 />
               </Form.Field>
               <Form.Field>
                 <Button
                   onClick={() => {
-                    setDate(null);
+                    setDate(undefined);
                   }}
                 >
                   No date
@@ -207,9 +215,12 @@ const TickModal = ({
                         showMonthDropdown
                         showYearDropdown
                         dropdownMode="select"
-                        selected={r.date && convertFromStringToDate(r.date)}
+                        selected={
+                          r.date ? convertFromStringToDate(r.date) : undefined
+                        }
                         onChange={(date) => {
-                          r.date = convertFromDateToString(date);
+                          // TODO: Stop mutating state directly here.
+                          r.date = convertFromDateToString(date) ?? r.date;
                           setRepeats([...repeats]);
                         }}
                       />
@@ -243,7 +254,7 @@ const TickModal = ({
                   icon="plus"
                   onClick={() => {
                     const repeat = {
-                      date: convertFromDateToString(new Date()),
+                      date: convertFromDateToString(new Date()) ?? initialDate,
                       comment: "",
                     };
                     if (repeats) {

src/utils/lazyRetry.ts
@@ -40,7 +40,7 @@ const lazyRetry = function (
 
             for (let i = 0; i < sessionStorage.length; i += 1) {
               const itemKey = sessionStorage.key(i);
-              if (!itemKey.startsWith("retry-lazy-refreshed/")) {
+              if (!itemKey || !itemKey.startsWith("retry-lazy-refreshed/")) {
                 continue;
               }
 

src/utils/polyline.ts
@@ -2,10 +2,15 @@ import { captureMessage } from "@sentry/core";
 import { hashHexColor } from "./colors";
 import { components } from "../@types/buldreinfo/swagger";
 
+type LatLngPair = Pick<
+  components["schemas"]["Coordinates"],
+  "latitude" | "longitude"
+>;
+
 export const parsePolyline = (
   polyline: string,
   onError?: (msg: string, extra?: Record<string, string>) => void,
-): components["schemas"]["Coordinates"][] => {
+): LatLngPair[] => {
   const reportError =
     onError ??
     ((message) => {
@@ -21,7 +26,7 @@ export const parsePolyline = (
   return polyline
     .split(";")
     .filter(Boolean)
-    .reduce((acc, value) => {
+    .reduce<LatLngPair[]>((acc, value) => {
       const latlng = value.split(",");
       if (latlng.length !== 2) {
         reportError("Wrong number of entries", { value });
@@ -39,7 +44,7 @@ export const parsePolyline = (
       }
 
       const last = acc[acc.length - 1];
-      if (lat === last?.[0] && lng === last?.[1]) {
+      if (lat === last?.latitude && lng === last?.longitude) {
         return acc;
       }
 

commit f19c373544c2ab99490d1c8b23987dc8b3b4bd1d

fix: Handle edge cases for SunOnWall & Sunrise
The "sun-on-wall" and "sunrise/sunset" widgets would have questionable
(and incorrect) behavior during exceptional times: midnight sun (and
presumably polar night) in the Arctic Circle.

This was previously handled, but a regression was introduced in 76e135f

This change also unbreaks the Storybook setup - when it was bumped to
Storybook v8, the Storybook v7 configuration files weren't completely
migrated. Maybe we can add some safety around this in a future PR to
prevent regressions.

.gitignore
@@ -4,3 +4,5 @@
 
 .yarnrc
 yarn-error.log
+
+*storybook.log

.storybook/main.ts
@@ -1,21 +1,47 @@
-import { StorybookConfig } from "@storybook/react-webpack5";
+import type { StorybookConfig } from "@storybook/react-webpack5";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
+    "@storybook/addon-webpack5-compiler-swc",
+    "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
+    "@chromatic-com/storybook",
     "@storybook/addon-interactions",
   ],
   framework: {
     name: "@storybook/react-webpack5",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
+  staticDirs: [{ from: "../build", to: "/" }],
 
-  staticDirs: ["../build"],
+  // NOTE: I have no idea why this is necessary - Storybook doesn't seem to be
+  //       picking up the default babel configurations, like it's supposed to
+  //       be doing. I wonder what happened?
+  webpackFinal: async (config) => {
+    config.module?.rules?.push({
+      test: /\.(jsx?|tsx?)$/,
+      exclude: [
+        /node_modules\/(?!(@react-leaflet|react-leaflet)\/)/i,
+        // Tree-shaking *should* make this unnecessary, but let's guard
+        // against accidental reverse-imports (a utility being import from a
+        // storybook file into production code).
+        /\.stories\.tsx?$/,
+      ],
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: [
+            "@babel/preset-env",
+            "@babel/preset-typescript",
+            // Runtime automatic with React 17+ allows not importing React in files only using JSX (no state or React methods)
+            ["@babel/preset-react", { runtime: "automatic" }],
+          ],
+        },
+      },
+    });
+    return config;
+  },
 };
 export default config;

.storybook/preview.ts
@@ -2,7 +2,6 @@ import { Preview } from "@storybook/react";
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,

src/components/common/widgets/SunOnWall/SunOnWall.stories.tsx
@@ -1,0 +1,40 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { SunOnWall } from "./SunOnWall";
+import { ComponentProps } from "react";
+
+export default {
+  title: "components/common/widgets/SunOnWall",
+  component: SunOnWall,
+  args: {
+    sunFromHour: 6,
+    sunToHour: 18,
+  } satisfies ComponentProps<typeof SunOnWall>,
+} satisfies Meta<typeof SunOnWall>;
+
+export const Default: StoryObj<typeof SunOnWall> = {};
+
+export const InvalidFrom: typeof Default = {
+  args: {
+    sunFromHour: -1,
+  },
+};
+
+export const InvalidTo: typeof Default = {
+  args: {
+    sunToHour: 25,
+  },
+};
+
+export const Flipped: typeof Default = {
+  args: {
+    sunFromHour: 18,
+    sunToHour: 6,
+  },
+};
+
+export const MidnightSun: typeof Default = {
+  args: {
+    sunFromHour: Number.NaN,
+    sunToHour: Number.NaN,
+  },
+};

src/components/common/widgets/SunOnWall/SunOnWall.tsx
@@ -1,0 +1,34 @@
+import { Image, Label, Popup } from "semantic-ui-react";
+
+type Props = {
+  sunFromHour: number;
+  sunToHour: number;
+};
+
+export const SunOnWall = ({ sunFromHour, sunToHour }: Props) => {
+  if (
+    sunFromHour <= 0 ||
+    sunToHour >= 24 ||
+    sunFromHour >= sunToHour ||
+    Number.isNaN(sunFromHour) ||
+    Number.isNaN(sunToHour)
+  ) {
+    return null;
+  }
+
+  return (
+    <Popup
+      content="Sun on wall"
+      trigger={
+        <Label image basic size="small">
+          <Image src="/svg/sun-on-wall.svg" alt="Sun on wall" size="mini" />
+          {String(sunFromHour).padStart(2, "0") +
+            ":00" +
+            " - " +
+            String(sunToHour).padStart(2, "0") +
+            ":00"}
+        </Label>
+      }
+    />
+  );
+};

src/components/common/widgets/SunOnWall/index.ts
@@ -1,0 +1,1 @@
+export { SunOnWall } from "./SunOnWall";

src/components/common/widgets/SunriseSunset/SunriseSunset.stories.tsx
@@ -1,0 +1,27 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { SunriseSunset } from "./SunriseSunset";
+import { ComponentProps } from "react";
+
+export default {
+  title: "components/common/widgets/SunriseSunset",
+  component: SunriseSunset,
+  args: {
+    lat: 69.75885227587338,
+    lng: 18.52388329795636,
+    date: new Date("2024-03-14T15:09:26.000Z"),
+  } satisfies ComponentProps<typeof SunriseSunset>,
+} satisfies Meta<typeof SunriseSunset>;
+
+export const Default: StoryObj<typeof SunriseSunset> = {};
+
+export const MidnightSun: typeof Default = {
+  args: {
+    date: new Date("2024-07-06T07:43:41.000Z"),
+  },
+};
+
+export const PolarNight: typeof Default = {
+  args: {
+    date: new Date("2024-12-14T15:09:26.000Z"),
+  },
+};

src/components/common/widgets/SunriseSunset/SunriseSunset.tsx
@@ -1,0 +1,38 @@
+import React from "react";
+import { Image, Label, Popup } from "semantic-ui-react";
+import SunCalc from "suncalc";
+
+type Props = {
+  lat: number;
+  lng: number;
+  date?: Date;
+};
+
+export const SunriseSunset = ({ lat, lng, date }: Props) => {
+  const { sunrise, sunset } = SunCalc.getTimes(date ?? new Date(), lat, lng);
+  if (Number.isNaN(sunrise.getTime()) || Number.isNaN(sunset.getTime())) {
+    return null;
+  }
+
+  return (
+    <Popup
+      content="Sunrise and sunset"
+      trigger={
+        <Label image basic size="small">
+          <Image
+            src="/svg/sunrise-sunset.svg"
+            alt="Sunrise and Sunset"
+            size="mini"
+          />
+          {String(sunrise.getHours()).padStart(2, "0") +
+            ":" +
+            String(sunrise.getMinutes()).padStart(2, "0") +
+            " - " +
+            String(sunset.getHours()).padStart(2, "0") +
+            ":" +
+            String(sunset.getMinutes()).padStart(2, "0")}
+        </Label>
+      }
+    />
+  );
+};

src/components/common/widgets/SunriseSunset/index.ts
@@ -1,0 +1,1 @@
+export { SunriseSunset } from "./SunriseSunset";

src/components/common/widgets/WeatherIcon.stories.tsx
@@ -1,7 +1,6 @@
-import type { MetaArgs } from "../../../utils/storybook";
 import { TWeatherSymbolKey } from "../../../yr";
 import { WeatherIcon } from "./widgets";
-import type { StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react";
 
 const meta = {
   title: "components/common/widgets/WeatherIcon",
@@ -9,7 +8,7 @@ const meta = {
   args: {
     symbol: undefined,
   },
-} satisfies MetaArgs<typeof WeatherIcon>;
+} satisfies Meta<typeof WeatherIcon>;
 
 type Story = StoryObj<typeof meta>;
 

src/components/common/widgets/widgets.tsx
@@ -7,14 +7,14 @@ import {
   Header,
   Message,
   Icon,
-  Image,
   Popup,
   Label,
   Button,
   LabelDetail,
 } from "semantic-ui-react";
-import SunCalc from "suncalc";
 import { TWeatherSymbolKey, weatherSymbolKeys } from "../../../yr";
+import { SunOnWall } from "./SunOnWall";
+import { SunriseSunset } from "./SunriseSunset";
 
 type LockSymbolProps = {
   lockedAdmin: boolean;
@@ -171,7 +171,6 @@ export function InsufficientPrivileges() {
     </Segment>
   );
 }
-
 type ConditionLabelsProps = {
   lat: number;
   lng: number;
@@ -183,6 +182,8 @@ type ConditionLabelsProps = {
   sunFromHour: number;
   sunToHour: number;
 };
+
+export { SunOnWall };
 
 export const WallDirection = ({
   wallDirectionCalculated,
@@ -204,30 +205,6 @@ export const WallDirection = ({
             <Icon name="compass outline" />
             {wallDirectionManual?.direction ??
               wallDirectionCalculated?.direction}
-          </Label>
-        }
-      />
-    );
-  }
-  return null;
-};
-
-export const SunOnWall = ({
-  sunFromHour,
-  sunToHour,
-}: Pick<ConditionLabelsProps, "sunFromHour" | "sunToHour">) => {
-  if (sunFromHour > 0 && sunToHour > 0) {
-    return (
-      <Popup
-        content="Sun on wall"
-        trigger={
-          <Label image basic size="small">
-            <Image src="/svg/sun-on-wall.svg" alt="Sun on wall" size="mini" />
-            {String(sunFromHour).padStart(2, "0") +
-              ":00" +
-              " - " +
-              String(sunToHour).padStart(2, "0") +
-              ":00"}
           </Label>
         }
       />
@@ -380,12 +357,6 @@ export function ConditionLabels({
   const d = new Date();
   const date = `${d.getFullYear()}.${(d.getMonth() + 1).toString().padStart(2, "0")}.${d.getDate()}`;
   const time = `${d.getHours().toString().padStart(2, "0")}:${d.getMinutes().toString().padStart(2, "0")}`;
-  const times = SunCalc.getTimes(new Date(), lat, lng);
-
-  // The library behaves incorrectly in the Arctic Circle, and the type
-  // definitions don't know about that.
-  // https://github.com/mourner/suncalc/issues/146
-  const sunrise: Date | string = times.sunrise;
 
   return (
     <>
@@ -394,27 +365,7 @@ export function ConditionLabels({
         wallDirectionManual={wallDirectionManual}
       />
       <SunOnWall sunFromHour={sunFromHour} sunToHour={sunToHour} />
-      {typeof sunrise !== "string" && (
-        <Popup
-          content="Sunrise and sunset"
-          trigger={
-            <Label image basic size="small">
-              <Image
-                src="/svg/sunrise-sunset.svg"
-                alt="Sunrise and Sunset"
-                size="mini"
-              />
-              {String(sunrise.getHours()).padStart(2, "0") +
-                ":" +
-                String(sunrise.getMinutes()).padStart(2, "0") +
-                " - " +
-                String(times.sunset.getHours()).padStart(2, "0") +
-                ":" +
-                String(times.sunset.getMinutes()).padStart(2, "0")}
-            </Label>
-          }
-        />
-      )}
+      <SunriseSunset lat={lat} lng={lng} />
       <YrLink lat={lat} lng={lng} />
       <Label
         href={`/webcams/` + JSON.stringify({ lat, lng, label })}

src/utils/storybook.d.ts
@@ -1,4 +1,0 @@
-import type { Meta } from "@storybook/react";
-import { ComponentProps } from "react";
-
-export type MetaArgs<T> = Meta<T> & { args: ComponentProps<T> };