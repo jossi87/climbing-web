9dfd7d5f2688bbf71c040b466535687d48d0823a
Evan Charlton <evancharlton@gmail.com>
2024-07-07 17:21:59 +0200

    chore: Fix TS strict errors around SectorEdit
    Focusing on `SectorEdit`, set `strict: true` in `tsconfig.json` and fix
    the detected errors.
    
    This shouldn't cause any changes to user-visible behavior (none were
    noticed in my local testing, though I can't promose it was 100%
    thorough). However, there are some under-the-hood improvements with
    Sentry reporting various "WTF?"-type situations, if they should occur.

--- src/components/SectorEdit/ProblemOrder.tsx
+++ src/components/SectorEdit/ProblemOrder.tsx
@@ -8,45 +8,54 @@ type Props = Pick<components["schemas"]["Sector"], "problemOrder"> & {
   ) => void;
 };
 
+/**
+ * This is just to work around the fact that _technically_ a Problem doesn't
+ * "need" to have an {@code id} field, since it's defined as optional in the
+ * Swagger-generated APIs. But we know that it'll be there and we can skip this.
+ */
+const TYPE_IMPOSSIBILITY = 0;
+
 export const ProblemOrder = ({ problemOrder, onChange }: Props) => {
-  const originalOrder = useRef(
+  const originalOrder: Record<number, number> = useRef(
     (problemOrder ?? []).reduce(
-      (acc, { id, nr }) => ({ ...acc, [id]: nr }),
+      (acc, { id, nr }) => ({ ...acc, [id ?? TYPE_IMPOSSIBILITY]: nr }),
       {},
     ),
   ).current;
 
-  return problemOrder.map(({ id, name, nr }) => {
-    const isModified = nr !== originalOrder[id];
-    const color = isModified ? "orange" : "grey";
-    return (
-      <Input
-        key={id}
-        size="small"
-        type="number"
-        step={1}
-        fluid
-        icon="hashtag"
-        iconPosition="left"
-        placeholder="Number"
-        defaultValue={nr}
-        label={{ basic: true, content: name, color }}
-        labelPosition="right"
-        onChange={(_, { value }) => {
-          const num = +value;
-          onChange(
-            problemOrder.map((problem) => {
-              if (problem.id === id) {
-                return {
-                  ...problem,
-                  nr: num,
-                };
-              }
-              return problem;
-            }),
-          );
-        }}
-      />
-    );
-  });
+  return (
+    problemOrder?.map(({ id, name, nr }) => {
+      const isModified = nr !== originalOrder[id ?? TYPE_IMPOSSIBILITY];
+      const color = isModified ? "orange" : "grey";
+      return (
+        <Input
+          key={id}
+          size="small"
+          type="number"
+          step={1}
+          fluid
+          icon="hashtag"
+          iconPosition="left"
+          placeholder="Number"
+          defaultValue={nr}
+          label={{ basic: true, content: name, color }}
+          labelPosition="right"
+          onChange={(_, { value }) => {
+            const num = +value;
+            onChange(
+              problemOrder.map((problem) => {
+                if (problem.id === id) {
+                  return {
+                    ...problem,
+                    nr: num,
+                  };
+                }
+                return problem;
+              }),
+            );
+          }}
+        />
+      );
+    }) ?? []
+  );
 };
