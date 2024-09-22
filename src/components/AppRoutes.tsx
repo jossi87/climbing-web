89854d96acfa401cf9ad3e55eb619c0cf7962e1b
Jostein Øygarden <jostein.oygarden@esmartsystems.com>
2024-09-22 10:29:44 +0200

    Remove old default scrollToTop behaviour. This is frustrating while looking on images on multi pitch routes with images on pitches as the page scrolls to top every time you open a media-modal.

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -109,112 +109,102 @@ const Webcams = lazy(
 );
 
 function AppRoutes() {
-  const Wrapper = ({ children }: { children: React.ReactNode }) => {
-    const location = useLocation();
-    useLayoutEffect(() => {
-      setTimeout(() => window.scrollTo(0, 0), 1);
-    }, [location.pathname]);
-    return children;
-  };
-
   return (
     <Suspense fallback={<Loading />}>
-      <Wrapper>
-        <Routes>
-          <Route path="/" element={<Frontpage />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/area/:areaId/:mediaId?" element={<Area />} />
-          <Route
-            path="/area/edit/:areaId"
-            element={
-              <AuthContainer level="admin">
-                <AreaEdit />
-              </AuthContainer>
-            }
-          />
-          <Route path="/areas" element={<Areas />} />
-          <Route path="/dangerous" element={<Dangerous />} />
-          <Route path="/donations" element={<Donations />} />
-          <Route path="/filter" element={<Problems filterOpen />} />
-          <Route path="/graph" element={<Graph />} />
-          <Route
-            path="/media/svg-edit/:mediaId"
-            element={
-              <AuthContainer level="admin">
-                <MediaSvgEdit />
-              </AuthContainer>
-            }
-          />
-          <Route
-            path="/permissions"
-            element={
-              <AuthContainer level="super-admin">
-                <Permissions />
-              </AuthContainer>
-            }
-          />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route
-            path="/problem/:problemId/:mediaId?/:pitch?"
-            element={<Problem />}
-          />
-          <Route
-            path="/problem/edit/media/:problemId"
-            element={
-              <AuthContainer level="logged-in">
-                <ProblemEditMedia />
-              </AuthContainer>
-            }
-          />
-          <Route
-            path="/problem/edit/:sectorId/:problemId"
-            element={
-              <AuthContainer level="admin">
-                <ProblemEdit />
-              </AuthContainer>
-            }
-          />
-          <Route
-            path="/problem/svg-edit/:problemId/:pitch/:mediaId"
-            element={
-              <AuthContainer level="admin">
-                <SvgEdit />
-              </AuthContainer>
-            }
-          />
-          <Route path="/problems" element={<Problems />} />
-          <Route path="/sites/:type" element={<Sites />} />
-          <Route path="/sector/:sectorId/:mediaId?" element={<Sector />} />
-          <Route
-            path="/sector/edit/:areaId/:sectorId"
-            element={
-              <AuthContainer level="admin">
-                <SectorEdit />
-              </AuthContainer>
-            }
-          />
-          <Route path="/ticks/:page" element={<Ticks />} />
-          <Route path="/swagger" element={<Swagger />} />
-          <Route
-            path="/trash"
-            element={
-              <AuthContainer level="super-admin">
-                <Trash />
-              </AuthContainer>
-            }
-          />
-          <Route
-            path="/user"
-            element={
-              <AuthContainer level="logged-in">
-                <Profile />
-              </AuthContainer>
-            }
-          />
-          <Route path="/user/:userId/:page?/:mediaId?" element={<Profile />} />
-          <Route path="/webcams/:json?" element={<Webcams />} />
-        </Routes>
-      </Wrapper>
+      <Routes>
+        <Route path="/" element={<Frontpage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/area/:areaId/:mediaId?" element={<Area />} />
+        <Route
+          path="/area/edit/:areaId"
+          element={
+            <AuthContainer level="admin">
+              <AreaEdit />
+            </AuthContainer>
+          }
+        />
+        <Route path="/areas" element={<Areas />} />
+        <Route path="/dangerous" element={<Dangerous />} />
+        <Route path="/donations" element={<Donations />} />
+        <Route path="/filter" element={<Problems filterOpen />} />
+        <Route path="/graph" element={<Graph />} />
+        <Route
+          path="/media/svg-edit/:mediaId"
+          element={
+            <AuthContainer level="admin">
+              <MediaSvgEdit />
+            </AuthContainer>
+          }
+        />
+        <Route
+          path="/permissions"
+          element={
+            <AuthContainer level="super-admin">
+              <Permissions />
+            </AuthContainer>
+          }
+        />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route
+          path="/problem/:problemId/:mediaId?/:pitch?"
+          element={<Problem />}
+        />
+        <Route
+          path="/problem/edit/media/:problemId"
+          element={
+            <AuthContainer level="logged-in">
+              <ProblemEditMedia />
+            </AuthContainer>
+          }
+        />
+        <Route
+          path="/problem/edit/:sectorId/:problemId"
+          element={
+            <AuthContainer level="admin">
+              <ProblemEdit />
+            </AuthContainer>
+          }
+        />
+        <Route
+          path="/problem/svg-edit/:problemId/:pitch/:mediaId"
+          element={
+            <AuthContainer level="admin">
+              <SvgEdit />
+            </AuthContainer>
+          }
+        />
+        <Route path="/problems" element={<Problems />} />
+        <Route path="/sites/:type" element={<Sites />} />
+        <Route path="/sector/:sectorId/:mediaId?" element={<Sector />} />
+        <Route
+          path="/sector/edit/:areaId/:sectorId"
+          element={
+            <AuthContainer level="admin">
+              <SectorEdit />
+            </AuthContainer>
+          }
+        />
+        <Route path="/ticks/:page" element={<Ticks />} />
+        <Route path="/swagger" element={<Swagger />} />
+        <Route
+          path="/trash"
+          element={
+            <AuthContainer level="super-admin">
+              <Trash />
+            </AuthContainer>
+          }
+        />
+        <Route
+          path="/user"
+          element={
+            <AuthContainer level="logged-in">
+              <Profile />
+            </AuthContainer>
+          }
+        />
+        <Route path="/user/:userId/:page?/:mediaId?" element={<Profile />} />
+        <Route path="/webcams/:json?" element={<Webcams />} />
+      </Routes>
     </Suspense>
   );
 }
