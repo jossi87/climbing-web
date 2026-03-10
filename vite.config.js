e1cf7ec4307e9a0ea82325533053537726361bb0
Jostein Øygarden <jostein.oygarden@esmartsystems.com>
2026-03-10 16:00:00 +0100

    css

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ export default defineConfig(({ mode }) => ({
     outDir: 'build',
     sourcemap: false,
     cssCodeSplit: true,
-    cssMinify: 'lightningcss',
+    cssMinify: 'esbuild',
     target: 'esnext',
     minify: 'esbuild',
     rollupOptions: {
