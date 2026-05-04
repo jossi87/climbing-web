239553009a29af21b13993743b68736cf9867aa3
Jostein Øygarden <jostein.oygarden@gmail.com>
2026-05-04 13:20:47 +0200

    Reduce React Query staleTime from 24h to 30 minutes

--- src/index.tsx
+++ src/index.tsx
@@ -93,7 +93,7 @@ export const Auth0ProviderWithNavigate = ({ children }: { children: ReactNode })
 };
 
 const queryClient = new QueryClient({
-  defaultOptions: { queries: { staleTime: 1000 * 60 * 60 * 24 } },
+  defaultOptions: { queries: { staleTime: 1000 * 60 * 30 } },
 });
 
 const Index = () => (
